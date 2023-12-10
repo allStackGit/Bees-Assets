6804032faf4eb4d492af3938dd1cc0b3e4c4fa2f
allStackGit <dmoran201@gmail.com>
2023-12-10 14:26:19 -0500

    Added frigate

--- Scripts/Level/LevelConstructor.cs
+++ Scripts/Level/LevelConstructor.cs
@@ -121,12 +121,12 @@ namespace Assets.Scripts.Level
                         //ConfigData.Ships.GetSavedSquads().FirstOrDefault((s) => s.Id == 41),  // human squad // 1 gunship #41
                         //ConfigData.Ships.GetSavedSquads().FirstOrDefault((s) => s.Id == 43),  // human squad // 3 gunships #43
                         //ConfigData.Ships.GetSavedSquads().FirstOrDefault((s) => s.Id == 45),  // human squad // 2 dreadnoughts #45
-                        //ConfigData.Ships.GetSavedSquads().FirstOrDefault((s) => s.Id == 47),  // human squad // 1 frigate #47
-                        ConfigData.Ships.GetSavedSquads().FirstOrDefault((s) => s.Id == 49),  // human squad // 1 barge #49
-                        ConfigData.Ships.GetSavedSquads().FirstOrDefault((s) => s.Id == 50),  // human squad // 1 cruiser #50
+                        ConfigData.Ships.GetSavedSquads().FirstOrDefault((s) => s.Id == 47),  // human squad // 1 frigate #47
+                        //ConfigData.Ships.GetSavedSquads().FirstOrDefault((s) => s.Id == 49),  // human squad // 1 barge #49
+                        //ConfigData.Ships.GetSavedSquads().FirstOrDefault((s) => s.Id == 50),  // human squad // 1 cruiser #50
                         ConfigData.Ships.GetSavedSquads().FirstOrDefault((s) => s.Id == 51),  // human squad // 1 flagship #51
-                        ConfigData.Ships.GetSavedSquads().FirstOrDefault((s) => s.Id == 52),  // human squad // 1 carrier #52
-                        ConfigData.Ships.GetSavedSquads().FirstOrDefault((s) => s.Id == 53),  // human squad // 1 fire ship #53
+                        //ConfigData.Ships.GetSavedSquads().FirstOrDefault((s) => s.Id == 52),  // human squad // 1 carrier #52
+                        //ConfigData.Ships.GetSavedSquads().FirstOrDefault((s) => s.Id == 53),  // human squad // 1 fire ship #53
                         //ConfigData.Ships.GetSavedSquads().FirstOrDefault((s) => s.Id == 58),  // human squad // 1 dreadnought #58
                         //ConfigData.Ships.GetSavedSquads().FirstOrDefault((s) => s.Id == 59),  // human squad // 4 scouts #59
 
