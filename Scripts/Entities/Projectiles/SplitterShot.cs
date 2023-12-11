f290e5ef13989fc659d259d75e7d073be39742a2
allStackGit <dmoran201@gmail.com>
2023-12-11 10:59:39 -0500

    Fixing bugs with projectiles and fixing prefabs

--- Scripts/Entities/Projectiles/SplitterShot.cs
+++ Scripts/Entities/Projectiles/SplitterShot.cs
@@ -16,7 +16,7 @@ namespace Assets.Scripts.Entities.Projectiles
             //Debugger.Log($"Split shot hit {target.name}");
             if (HasExplosion)
             {
-                Explosion =  Instantiate(ExplosionPrefab, GetPosition(), Quaternion.identity);
+                Explosion =  Instantiate(ExplosionPrefab, GetPosition() + (Vector2) Level.transform.position, Quaternion.identity);
                 Explosion.transform.parent = Level.Map.transform;
             }
             Split(target);
