ec732c21fcb7644a6363f7f0130cd8c8db43549b
allStackGit <dmoran201@gmail.com>
2026-08-08 17:29:04 -0400

    Preserve map index compatibility for Titania

--- Scripts/ConfigData.cs
+++ Scripts/ConfigData.cs
@@ -432,10 +432,10 @@ namespace Assets.Scripts
             { ShipTypes.WarpGate,      new Vector2Int(382, 238)},
 
             { ShipTypes.Beehive,       new Vector2Int(0, 0)},
-            { ShipTypes.Bumblebee,     new Vector2Int(0, 0)},
+            { ShipTypes.Bumblebee,      new Vector2Int(0, 0)},
             { ShipTypes.CarpenterBee,   new Vector2Int(0, 0)},
-            { ShipTypes.Honeybee,      new Vector2Int(0, 0)},
-            { ShipTypes.Hornet,        new Vector2Int(0, 0)},
+            { ShipTypes.Honeybee,       new Vector2Int(0, 0)},
+            { ShipTypes.Hornet,         new Vector2Int(0, 0)},
             { ShipTypes.Leafcutter,     new Vector2Int(0, 0)},
             { ShipTypes.Queen,         new Vector2Int(0, 0)},
             { ShipTypes.Wasp,          new Vector2Int(0, 0)},
@@ -458,10 +458,10 @@ namespace Assets.Scripts
             { ShipTypes.WarpGate,      Huge},
 
             { ShipTypes.Beehive,       Enormous},
-            { ShipTypes.Bumblebee,     Large},
+            { ShipTypes.Bumblebee,      Large},
             { ShipTypes.CarpenterBee,   Huge},
-            { ShipTypes.Honeybee,      Tiny},
-            { ShipTypes.Hornet,        Tiny},
+            { ShipTypes.Honeybee,       Tiny},
+            { ShipTypes.Hornet,         Tiny},
             { ShipTypes.Leafcutter,     Medium},
             { ShipTypes.Queen,         Unfathomable},
             { ShipTypes.Wasp,          Small},
@@ -558,8 +558,8 @@ namespace Assets.Scripts
             { ShipTypes.Honeybee,       new Color[] {UnsetColor } },
             { ShipTypes.Hornet,         new Color[] {UnsetColor } },
             { ShipTypes.Leafcutter,     new Color[] {UnsetColor } },
-            { ShipTypes.Queen,          new Color[] {UnsetColor } },
-            { ShipTypes.Wasp,           new Color[] {UnsetColor } },
+            { ShipTypes.Queen,         new Color[] {UnsetColor } },
+            { ShipTypes.Wasp,          new Color[] {UnsetColor } },
             { ShipTypes.YellowJacket,   new Color[] {UnsetColor } },
         };
 
@@ -670,8 +670,8 @@ namespace Assets.Scripts
         public static readonly List<Data.Map> Maps = new List<Data.Map> { 
             new Data.Map(0, new Vector2(0, -230), new Vector2(0, 230), Locations.Pluto),
             new Data.Map(1, new Vector2(0, -430), new Vector2(0, 430), Locations.Neptune),
-            new Data.Map(2, new Vector2(0, -215), new Vector2(0, 215), Locations.Titania),
-            new Data.Map(3, new Vector2(0, -430), new Vector2(0, 430), Locations.Uranus),
+            new Data.Map(2, new Vector2(0, -430), new Vector2(0, 430), Locations.Uranus),
+            new Data.Map(3, new Vector2(0, -215), new Vector2(0, 215), Locations.Titania),
             
         };
         public static readonly List<Data.ObstacleMap> ObstacleMaps = new List<Data.ObstacleMap> { 
@@ -1512,7 +1512,7 @@ namespace Assets.Scripts
         {
             _challengeSavedSquadsData = new SavedSquadsData(shouldFileExist, 2);
         }
-        public static SavedSquadData GetSavedSquadsData()
+        public static SavedSquadsData GetSavedSquadsData()
         {
             return _savedSquadsData;
         }
