633076e11dfaff84d3a73313e9d0dc987a67ffe3
allStackGit <dmoran201@gmail.com>
2026-08-08 10:51:34 -0400

    Preserve unsigned matchup IDs as strings

--- Scripts/Server/CommandResponse.cs
+++ Scripts/Server/CommandResponse.cs
@@ -6,12 +6,13 @@ namespace Assets.Scripts.Server
     public class CommandResponse : ServerResponse
     {
         public string Name, ShootingStrategyName, MatchupString, ShootingStrategyMatchupString;
+        public string MatchupId, ShootingStrategyMatchupId;
         public int StrategyId, ShootingStrategyId;
-        public long MatchupId, OutcomeId, ShootingStrategyMatchupId, ShootingStrategyOutcomeId;
+        public long OutcomeId, ShootingStrategyOutcomeId;
         public bool IsCached;
        
         public CommandResponse(string type, int status, int hash, float serverLatency, string name, string shootingStrategyName, string matchupString, string shootingStrategyMatchupString, int strategyId, int shootingStrategyId,
-            long matchupId, long shootingStrategyMatchupId, long outcomeId, long shootingStrategyOutcomeId, bool isCached) : base(type, status, hash, serverLatency)
+            string matchupId, string shootingStrategyMatchupId, long outcomeId, long shootingStrategyOutcomeId, bool isCached) : base(type, status, hash, serverLatency)
         {
             Name = name;
             ShootingStrategyName = shootingStrategyName;
