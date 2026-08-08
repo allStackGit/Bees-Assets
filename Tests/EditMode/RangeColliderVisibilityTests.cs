1889cfcaf0e69e289cd77c827fbe2e00ca0a2118
allStackGit <dmoran201@gmail.com>
2026-08-08 09:08:03 -0400

    Add overlapping range visibility ownership regression

--- Tests/EditMode/RangeColliderVisibilityTests.cs
+++ Tests/EditMode/RangeColliderVisibilityTests.cs
@@ -28,31 +28,15 @@ namespace Bees.Tests.EditMode
         [Test]
         public void ExitingMapObjectRemovesTheExitedObjectNotTheLastEnteredObject()
         {
-            GameObject levelObject = CreateObject("Visibility Level");
-            object level = levelObject.AddComponent(RuntimeAssembly.GetType("Assets.Scripts.Levels.Level"));
-            object state = levelObject.AddComponent(RuntimeAssembly.GetType("Assets.Scripts.Levels.GameState"));
-            RuntimeAssembly.SetField(level, "State", state);
-            RuntimeAssembly.SetField(state, "Level", level);
-
-            GameObject shipObject = CreateObject("Visibility Ship");
-            object ship = shipObject.AddComponent(RuntimeAssembly.GetType("Assets.Scripts.Entities.Ships.Ship"));
-            RuntimeAssembly.SetField(ship, "Level", level);
-
-            GameObject weaponObject = CreateObject("Visibility Weapon");
-            object weapon = weaponObject.AddComponent(RuntimeAssembly.GetType("Assets.Scripts.Entities.Ships.Weapons.Weapon"));
-            RuntimeAssembly.SetField(weapon, "Ship", ship);
-
-            GameObject rangeObject = CreateObject("Visibility Range");
-            object rangeCollider = rangeObject.AddComponent(RuntimeAssembly.GetType("Assets.Scripts.Entities.Ships.Weapons.RangeCollider"));
-            RuntimeAssembly.SetField(rangeCollider, "Weapon", weapon);
-
-            ObjectFixture first = CreateMapObject("First visible object");
-            ObjectFixture second = CreateMapObject("Second visible object");
+            LevelFixture level = CreateLevel();
+            object rangeCollider = CreateRangeCollider(level.Level, "Primary range");
+            ObjectFixture first = CreateMapObject("First visible object", level.Level);
+            ObjectFixture second = CreateMapObject("Second visible object", level.Level);
 
             RuntimeAssembly.Invoke(rangeCollider, "OnTriggerEnter2D", first.Collider);
             RuntimeAssembly.Invoke(rangeCollider, "OnTriggerEnter2D", second.Collider);
 
-            object visibleObjects = RuntimeAssembly.GetField(state, "PlayerVisibleMapObjects");
+            object visibleObjects = RuntimeAssembly.GetField(level.State, "PlayerVisibleMapObjects");
             Assert.That(RuntimeAssembly.GetCount(visibleObjects), Is.EqualTo(2));
 
             RuntimeAssembly.Invoke(rangeCollider, "OnTriggerExit2D", first.Collider);
@@ -65,13 +49,62 @@ namespace Bees.Tests.EditMode
                 "The exited object must not remain in PlayerVisibleMapObjects.");
         }
 
-        private ObjectFixture CreateMapObject(string name)
+        [Test]
+        public void MapObjectRemainsVisibleUntilEveryObservingWeaponRangeHasExited()
+        {
+            LevelFixture level = CreateLevel();
+            object firstRange = CreateRangeCollider(level.Level, "First observing range");
+            object secondRange = CreateRangeCollider(level.Level, "Second observing range");
+            ObjectFixture target = CreateMapObject("Shared visible object", level.Level);
+            object visibleObjects = RuntimeAssembly.GetField(level.State, "PlayerVisibleMapObjects");
+
+            RuntimeAssembly.Invoke(firstRange, "OnTriggerEnter2D", target.Collider);
+            RuntimeAssembly.Invoke(secondRange, "OnTriggerEnter2D", target.Collider);
+            Assert.That(RuntimeAssembly.GetCount(visibleObjects), Is.EqualTo(1));
+
+            RuntimeAssembly.Invoke(firstRange, "OnTriggerExit2D", target.Collider);
+            Assert.That(CollectionContains(visibleObjects, target.MapObject), Is.True,
+                "One weapon leaving range must not hide an object still observed by another weapon.");
+
+            RuntimeAssembly.Invoke(secondRange, "OnTriggerExit2D", target.Collider);
+            Assert.That(CollectionContains(visibleObjects, target.MapObject), Is.False,
+                "The object should be removed after its final observing range exits.");
+        }
+
+        private LevelFixture CreateLevel()
+        {
+            GameObject levelObject = CreateObject("Visibility Level");
+            object level = levelObject.AddComponent(RuntimeAssembly.GetType("Assets.Scripts.Levels.Level"));
+            object state = levelObject.AddComponent(RuntimeAssembly.GetType("Assets.Scripts.Levels.GameState"));
+            RuntimeAssembly.SetField(level, "State", state);
+            RuntimeAssembly.SetField(state, "Level", level);
+            return new LevelFixture(level, state);
+        }
+
+        private object CreateRangeCollider(object level, string name)
+        {
+            GameObject shipObject = CreateObject(name + " Ship");
+            object ship = shipObject.AddComponent(RuntimeAssembly.GetType("Assets.Scripts.Entities.Ships.Ship"));
+            RuntimeAssembly.SetField(ship, "Level", level);
+
+            GameObject weaponObject = CreateObject(name + " Weapon");
+            object weapon = weaponObject.AddComponent(RuntimeAssembly.GetType("Assets.Scripts.Entities.Ships.Weapons.Weapon"));
+            RuntimeAssembly.SetField(weapon, "Ship", ship);
+
+            GameObject rangeObject = CreateObject(name);
+            object rangeCollider = rangeObject.AddComponent(RuntimeAssembly.GetType("Assets.Scripts.Entities.Ships.Weapons.RangeCollider"));
+            RuntimeAssembly.SetField(rangeCollider, "Weapon", weapon);
+            return rangeCollider;
+        }
+
+        private ObjectFixture CreateMapObject(string name, object level)
         {
             GameObject gameObject = CreateObject(name);
             gameObject.tag = "Object";
             Collider2D collider = gameObject.AddComponent<BoxCollider2D>();
             object mapObject = gameObject.AddComponent(RuntimeAssembly.GetType("MapObject"));
             RuntimeAssembly.SetField(mapObject, "Id", _nextMapObjectId++);
+            RuntimeAssembly.SetField(mapObject, "Level", level);
             return new ObjectFixture(mapObject, collider);
         }
 
@@ -92,6 +125,18 @@ namespace Bees.Tests.EditMode
                 }
             }
             return false;
+        }
+
+        private sealed class LevelFixture
+        {
+            public readonly object Level;
+            public readonly object State;
+
+            public LevelFixture(object level, object state)
+            {
+                Level = level;
+                State = state;
+            }
         }
 
         private sealed class ObjectFixture
