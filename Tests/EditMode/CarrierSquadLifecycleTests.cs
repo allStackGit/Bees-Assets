93b6c16ae786f30599a3bdc4ec20ff80cadf511d
allStackGit <dmoran201@gmail.com>
2026-08-09 19:34:31 -0400

    Test striker return before bombing completion

--- Tests/EditMode/CarrierSquadLifecycleTests.cs
+++ Tests/EditMode/CarrierSquadLifecycleTests.cs
@@ -21,5 +21,22 @@ namespace Bees.Tests.EditMode
 
             StringAssert.Contains("IsDroneSquad ? ConfigData.Configuration.CarrierCarryDroneMax : ConfigData.Configuration.CarrierCarryStrikerMax", source);
         }
+
+        [Test]
+        public void StrikerBombingRunWaitsForCarrierReturnBeforeFinalizing()
+        {
+            string path = Path.Combine(Application.dataPath, "Scripts", "Levels", "Commands", "BombingRun.cs");
+            string source = File.ReadAllText(path);
+
+            int endBombingRun = source.IndexOf("private void EndBombingRun()");
+            int waitForReturn = source.IndexOf("if (!HaveAllShipsFinished(_endBombingRun_ships))", endBombingRun);
+            int earlyReturn = source.IndexOf("return;", waitForReturn);
+            int finalize = source.IndexOf("SetFinalize(\"Completed bombing run\")", endBombingRun);
+
+            Assert.That(endBombingRun, Is.GreaterThanOrEqualTo(0));
+            Assert.That(waitForReturn, Is.GreaterThan(endBombingRun));
+            Assert.That(earlyReturn, Is.GreaterThan(waitForReturn));
+            Assert.That(finalize, Is.GreaterThan(earlyReturn));
+        }
     }
 }
