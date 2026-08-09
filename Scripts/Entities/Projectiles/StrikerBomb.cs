359ec68a0e3d8ead45d2cfbfcd1b9b0c0d482492
allStackGit <dmoran201@gmail.com>
2026-08-09 07:51:37 -0400

    Keep striker bomb ownership through delayed damage

--- Scripts/Entities/Projectiles/StrikerBomb.cs
+++ Scripts/Entities/Projectiles/StrikerBomb.cs
@@ -14,11 +14,13 @@ namespace Assets.Scripts.Entities.Projectiles
         private ScaledTimer _damageTimer = new ScaledTimer();
         public AudioSource BombReleaseSound;
         public AudioSource BombExplosionSound;
-        // Use this for initialization
+        private long _shooterFleetShipId;
+
         public void Setup(Level level, Weapon weapon, Ship shooter, Ship target, Vector2 startingPosition, float angle, int range, int power, Ship contactedShip)
         {
             base.Setup(level, weapon, shooter, target, startingPosition, angle, range, power);
             ContactedShip = contactedShip;
+            _shooterFleetShipId = FleetShip.Id;
             transform.parent = ContactedShip.transform;
             _killSequenceTimer.Reuse(1.5f, KillSequence);
             Level.AddTimer(_killSequenceTimer);
@@ -26,13 +28,19 @@ namespace Assets.Scripts.Entities.Projectiles
             {
                 BombReleaseSound.Play();
             }
-            //Invoke(nameof(KillSequence), 1.5f);
         }
 
+        public override void ClearData()
+        {
+            base.ClearData();
+            ContactedShip = null;
+            _shooterFleetShipId = 0;
+        }
 
         public override void KillSequence()
         {
-            if (!ContactedShip.IsDead)
+            Level.CancelTimer(_killSequenceTimer);
+            if (ContactedShip != null && !ContactedShip.IsDead)
             {
                 if (!Level.Stage.IsTraining)
                 {
@@ -45,31 +53,53 @@ namespace Assets.Scripts.Entities.Projectiles
                     }
                 }
 
-                _damageTimer.Reuse(.5f, Damage);
+                // Keep this projectile out of the pool until its delayed damage has
+                // resolved. Returning it here allowed Setup on a new shot to overwrite
+                // ContactedShip/Power/Shooter before Damage ran.
+                Deactivate();
+                _damageTimer.Reuse(.5f, DamageAndKill);
                 Level.AddTimer(_damageTimer);
-                //Invoke(nameof(Damage), .5f);
-                Kill();
             }
             else
             {
                 Kill();
             }
         }
+
         public override void Kill()
         {
             Level.CancelTimer(_killSequenceTimer);
+            Level.CancelTimer(_damageTimer);
             base.Kill();
         }
+
+        private void DamageAndKill()
+        {
+            Damage();
+            Kill();
+        }
+
         public void Damage()
         {
-            if (!ContactedShip.IsDead)
+            if (ContactedShip == null || ContactedShip.IsDead)
+            {
+                return;
+            }
+
+            // Ship wrappers are pooled independently of projectiles. If the original
+            // Striker has already been recycled, preserve the explosion damage without
+            // attributing it to the new occupant of that pooled Ship object.
+            if (Shooter != null && Shooter.FleetShip != null && Shooter.FleetShip.Id == _shooterFleetShipId)
             {
                 Ship.LogAttackingDamage(Power, Shooter, FleetShip, SavedSquad, ContactedShip);
+            }
+            else
+            {
+                ContactedShip.LogDamage(Power);
             }
         }
         protected override void FixedUpdate()
         {
-
         }
     }
 }
