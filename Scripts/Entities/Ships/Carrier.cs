67120cc51c16499b3c5012090de41da0ad45869e
allStackGit <dmoran201@gmail.com>
2026-08-09 08:06:58 -0400

    Detach carrier craft before pooled carrier reuse

--- Scripts/Entities/Ships/Carrier.cs
+++ Scripts/Entities/Ships/Carrier.cs
@@ -1,10 +1,45 @@
-﻿
-using UnityEngine;
+using Assets.Scripts.Data;
+using Assets.Scripts.Levels;
+using System.Linq;
 
 namespace Assets.Scripts.Entities.Ships
 {
     public class Carrier : Ship
     {
-        
+        public override void Kill(Ship killer, FleetShip killerFleetShip, SavedSquad killerSavedSquad, bool endKill = false)
+        {
+            if (!IsDead)
+            {
+                Carrier replacementCarrier = Level.State.GetHumanShips()
+                    .OfType<Carrier>()
+                    .FirstOrDefault(carrier => carrier != this && !carrier.IsDead);
+
+                foreach (CarrierShip carrierShip in Level.State.GetHumanShips()
+                    .OfType<CarrierShip>()
+                    .Where(ship => ship.Carrier == this)
+                    .ToList())
+                {
+                    if (replacementCarrier != null)
+                    {
+                        carrierShip.Carrier = replacementCarrier;
+                    }
+                    else
+                    {
+                        if (carrierShip is Striker striker)
+                        {
+                            striker.LastCarrierPosition = GetPosition();
+                        }
+                        carrierShip.Carrier = null;
+                    }
+
+                    if (carrierShip.Squad is CarrierSquad carrierSquad)
+                    {
+                        carrierSquad.Carrier = replacementCarrier;
+                    }
+                }
+            }
+
+            base.Kill(killer, killerFleetShip, killerSavedSquad, endKill);
+        }
     }
 }
