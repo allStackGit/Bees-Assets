f77dde57958a3d1560e6895b7dbc8b2d899b1234
allStackGit <dmoran201@gmail.com>
2026-08-09 13:41:33 -0400

    Align campaign catalog test with isolated Pluto 4

--- Tests/EditMode/CampaignMissionCatalogTests.cs
+++ Tests/EditMode/CampaignMissionCatalogTests.cs
@@ -27,7 +27,7 @@ namespace Bees.Tests.EditMode
 
         private static readonly string[] ExpectedSetupMethods =
         {
-            "Pluto1Anomaly", "Pluto2Reinforcements", "Pluto3Pushback", "Pluto4BluerPastures",
+            "Pluto1Anomaly", "Pluto2Reinforcements", "Pluto3Pushback", "Pluto4BluerPasturesCampaign",
             "Neptune1SeizeTheMeans", "Neptune2OfProduction", "Neptune3PressingForwardCampaign",
             "Titania1MinesweeperCampaign", "Titania2BeenocularsCampaign", "Uranus1OnTheOffensive",
             "Uranus2OnTheDefensive", "Uranus3ANewThreat"
