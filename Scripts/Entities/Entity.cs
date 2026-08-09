07c8415c5cc8f49a7f2579a1ee4aa741a4af2bec
allStackGit <dmoran201@gmail.com>
2026-08-09 09:54:28 -0400

    Preserve Unity null semantics for entities

--- Scripts/Entities/Entity.cs
+++ Scripts/Entities/Entity.cs
@@ -12,7 +12,6 @@ namespace Assets.Scripts.Entities
     {
         public long Id;
         public long Age;
-        //public readonly DateTime StartTime = DateTime.Now;
         public Level Level;
         public Stage Stage;
         public int Side;
@@ -21,20 +20,8 @@ namespace Assets.Scripts.Entities
         public Rigidbody2D Body;
         public SpriteRenderer SpriteRenderer;
         public Transform Transform;
-        /// <summary>
-        /// The rotation of the entity, in degrees
-        /// </summary>
         public float OriginalRotation, Rotation;
-        //protected virtual void Update() // [debug] [training] Should probably just remove this during training
-        //{
-        //    if (!Level.IsTraining && !Level.IsPaused)
-        //    {
-        //        Age++;
-        //    }
-        //}        
-        //public int GetLifeTime() {
-        //    return DateTime.Now.Subtract(StartTime).Seconds;
-        //}
+
         public virtual void Create(Stage stage)
         {
             Stage = stage;
@@ -57,11 +44,6 @@ namespace Assets.Scripts.Entities
         {
             return AngleToPoint(entity.GetPosition());
         }
-        /// <summary>
-        /// Calculates the angle to a point in radians
-        /// </summary>
-        /// <param name="point"></param>
-        /// <returns></returns>
         public float AngleToPoint(Vector2 point) 
         {
             return Utilities.AngleBetweenPoints(GetPosition(), point);
@@ -70,46 +52,12 @@ namespace Assets.Scripts.Entities
         {
             return Utilities.DirectionBetweenPoints(GetPosition(), point);
         }
-        //private float _degrees;
-        //private Vector2 _direction;
         private Vector2 _position;
-        /// <summary>
-        /// Gets the angle (0-360) in degrees
-        /// </summary>
-        /// <param name="point"></param>
-        /// <returns></returns>
         public float GetDegreesTowardsPoint(Vector2 point)
         {
-            //_degrees = AngleToPoint(point) * Mathf.Rad2Deg;
-            ////Debug.Log($"Angle towards movement point before adjustment {degrees}");
-            //if (_degrees > 0) // if the angle is greater than PI, subtract 2 PI to get the equivilent negative angle
-            //{
-            //    _degrees = Mathf.Abs(_degrees - 180);
-
-            //}
-            //else if (_degrees < 0) // if the angle is less than negative PI, add 2 PI to get the equivilent negative angle
-            //{
-            //    _degrees = Mathf.Abs(_degrees) + 180;
-            //}
-            ////Debug.Log($"Angle towards movement point after adjustment {_degrees}");
-            //return _degrees;
-
             _position = GetPosition();
             return Mathf.Repeat(-Mathf.Atan2(point.x - _position.x, point.y - _position.y) * Mathf.Rad2Deg, 360f);
-
-
-
         }
-        //private float _result;
-        //public float GetRotatedAngleToPoint(Vector2 point)
-        //{
-        //    _result = GetDegreesTowardsPoint(point) - GetRotation();
-        //    if (_result < 0)
-        //    {
-        //        _result += 360;
-        //    }
-        //    return _result;
-        //}
         public bool IsFriendly(Entity entity)
         {
             return Side == entity.Side;
@@ -130,14 +78,17 @@ namespace Assets.Scripts.Entities
         private Entity _entity;
         public override bool Equals(System.Object obj)
         {
-            if (obj == null)
+            if (System.Object.ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if ((UnityEngine.Object)this == null || obj == null)
             {
                 return false;
             }
 
-            // If parameter cannot be cast to class return false.
             _entity = obj as Entity;
-            if (_entity == null)
+            if ((UnityEngine.Object)_entity == null)
             {
                 return false;
             }
@@ -147,6 +98,14 @@ namespace Assets.Scripts.Entities
 
         public bool Equals(Entity other)
         {
+            if (System.Object.ReferenceEquals(this, other))
+            {
+                return true;
+            }
+            if ((UnityEngine.Object)this == null || (UnityEngine.Object)other == null)
+            {
+                return false;
+            }
             return Id == other.Id;
         }
 
@@ -157,16 +116,17 @@ namespace Assets.Scripts.Entities
 
         public static bool operator ==(Entity a, Entity b)
         {
-            // If both are null, or both are same instance, return true.
+            bool aIsUnityNull = System.Object.ReferenceEquals(a, null) || (UnityEngine.Object)a == null;
+            bool bIsUnityNull = System.Object.ReferenceEquals(b, null) || (UnityEngine.Object)b == null;
+
+            if (aIsUnityNull || bIsUnityNull)
+            {
+                return aIsUnityNull && bIsUnityNull;
+            }
+
             if (System.Object.ReferenceEquals(a, b))
             {
                 return true;
-            }
-
-            // If one is null, but not both, return false.
-            if (((object)a == null) || ((object)b == null))
-            {
-                return false;
             }
 
             return a.Id == b.Id;
@@ -178,8 +138,6 @@ namespace Assets.Scripts.Entities
         }
         public virtual void Activate()
         {
-            //gameObject.SetActive(true);
-            //Collider.enabled = true;
             Body.simulated = true;
             if (Stage.IsRendering)
             {
@@ -189,8 +147,6 @@ namespace Assets.Scripts.Entities
         }
         public virtual void Deactivate()
         {
-            //gameObject.SetActive(false);
-            //Collider.enabled = false;
             Body.simulated = false;
             if (Stage.IsRendering)
             {
@@ -199,6 +155,4 @@ namespace Assets.Scripts.Entities
             enabled = false;
         }
     }
-
 }
-
