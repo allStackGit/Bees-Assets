81785ca8cdefcfaa77aa50b8edbbda9725aae941
allStackGit <dmoran201@gmail.com>
2026-08-09 08:07:39 -0400

    Handle carrier loss without stale pooled references

--- Scripts/Entities/Ships/Striker.cs
+++ Scripts/Entities/Ships/Striker.cs
@@ -1,4 +1,4 @@
-﻿using Assets.Scripts;
+using Assets.Scripts;
 using Assets.Scripts.Data;
 using Assets.Scripts.Entities;
 using Assets.Scripts.Entities.Projectiles;
@@ -7,30 +7,18 @@ using Assets.Scripts.Levels;
 using Assets.Scripts.Levels.Commands;
 using System.Collections.Generic;
 using System.Linq;
-
 using UnityEngine;
 
 namespace Assets.Scripts.Entities.Ships
 {
     public class Striker : CarrierShip
     {
-        /// <summary>
-        /// If the striker's bomb is loaded and ready, if not, this means it has dropped it and must return to the carrier before pursuing another target
-        /// </summary>
         public bool IsBombReady;
-        /// <summary>
-        /// Has the striker either dropped its bomb on its target it, or doesn't have a bomb and is going back to the carrier, or the whole target squad is dead
-        /// </summary>
         public bool HasCompletedRun;
-        /// <summary>
-        /// Has the striker dropped its bomb on its target
-        /// </summary>
         public bool HasDroppedBomb;
         public bool HasReturnedToCarrier;
         public GameObject LoadedIndicator, CarriedBomb;
-        //public Vector2 IndicatorOffset;
         private SpriteRenderer _indicatorSprite;
-        //private GameObject _droppedBomb;
         public Ship ContactedShip, TouchingShip;
         public Bomb Bomb;
         public Vector2 LastCarrierPosition;
@@ -38,7 +26,6 @@ namespace Assets.Scripts.Entities.Ships
         public override void Create(Stage stage)
         {
             base.Create(stage);
-
             if (Stage.IsTraining)
             {
                 Destroy(LoadedIndicator);
@@ -49,8 +36,8 @@ namespace Assets.Scripts.Entities.Ships
             }
             Bomb = (Bomb)Weapons.First();
             IsBomber = true;
-
         }
+
         private ScaledTimer _checkCarrierReloadTimer = new ScaledTimer();
         public override void Setup(Level level, FleetShip fleetShip, Squad squad, Vector2 offsetFromCenter)
         {
@@ -58,8 +45,8 @@ namespace Assets.Scripts.Entities.Ships
             SetIndicatorColor();
             _checkCarrierReloadTimer.Reuse(1, CheckCarrierReload, true);
             Level.AddTimer(_checkCarrierReloadTimer);
-            //InvokeRepeating(nameof(CheckCarrierReload), 1, 1);
         }
+
         public override void ClearData()
         {
             base.ClearData();
@@ -70,6 +57,7 @@ namespace Assets.Scripts.Entities.Ships
             TouchingShip = null;
             LastCarrierPosition = Vector2.zero;
         }
+
         public override void Deactivate()
         {
             base.Deactivate();
@@ -78,6 +66,7 @@ namespace Assets.Scripts.Entities.Ships
                 LoadedIndicator.SetActive(false);
             }
         }
+
         public override void Activate()
         {
             base.Activate();
@@ -86,6 +75,7 @@ namespace Assets.Scripts.Entities.Ships
                 LoadedIndicator.SetActive(true);
             }
         }
+
         private GameObject _collidingThing;
         private Ship _collidingShip;
         public override bool IsCloseEnoughToTargetCoordinates(float distance)
@@ -93,38 +83,28 @@ namespace Assets.Scripts.Entities.Ships
             return distance < ConfigData.ShipTurningRadius && !(Squad.HasOnlyBombers && !IsFollowingPath && HasTargetEnemyShipToFollow && Squad.HasCommand && Squad.GetCommand().CommandType == ConfigData.CommandTypes.BombingRun
                 && ProximityCollider.NearbyEnemyShips.Contains(TargetEnemyShipToFollow));
         }
-        protected override void OnTriggerEnter2D(Collider2D collider) // projectile collision
+
+        protected override void OnTriggerEnter2D(Collider2D collider)
         {
             _collidingThing = collider.gameObject;
-            
             if (_collidingThing.CompareTag("Ship") && Collider.IsTouching(collider))
             {
                 TouchingShip = _collidingThing.GetComponent<Ship>();
-                //Debug.Log($"Striker collided with a ship!" +
-                //    $"{TouchingShip}, " +
-                //    $"{Squad}, " +
-                //    $"{TargetShips.First()}");
-
                 if (TouchingShip.Side != Side && Squad.HasCommand && Bomb.TargetShip == TouchingShip && IsBombReady)
                 {
-                    //Debug.Log($"Collided with our target {TouchingShip.Name}!");
                     ContactedShip = TouchingShip;
                     DropBomb();
-
                 }
             }
             else if (IsUserControlled && _collidingThing.name == "Selection Box")
             {
-                //Debug.Log("Striker hit selection box");
                 Stage.Selector.SelectShip(this);
             }
-
         }
+
         protected override void OnTriggerExit2D(Collider2D collider)
         {
             _collidingThing = collider.gameObject;
-            // OnTriggerExit2D is itself the evidence that contact ended. Requiring
-            // Collider.IsTouching here prevents stale TouchingShip state from clearing.
             if (TouchingShip != null && _collidingThing.CompareTag("Ship"))
             {
                 _collidingShip = _collidingThing.GetComponent<Ship>();
@@ -138,29 +118,24 @@ namespace Assets.Scripts.Entities.Ships
                 Stage.Selector.DeselectShip(this);
             }
         }
+
         public void TryToDropBombs()
         {
-            //Debug.Log($"Trying to drop bombs with {Name}");
             if (TouchingShip != null && TouchingShip.Side != Side && IsBombReady)
             {
                 ContactedShip = TouchingShip;
                 DropBomb();
-                return;
             }
-            //Debug.Log($"Failed trying to drop bombs with {Name}: TouchingShip: [{TouchingShip}], BombsReady: {BombsReady}");
-
         }
+
         private void CheckCarrierReload()
         {
-            if (!Carrier.IsDead && DistanceTo(Carrier) < 15 && !IsBombReady)
+            if (Carrier != null && !Carrier.IsDead && DistanceTo(Carrier) < 15 && !IsBombReady)
             {
                 SetBombsReadyStatus(true);
             }
         }
-        /// <summary>
-        /// Sets the status of the bomb (loaded or not) and sets the indicator accordingly
-        /// </summary>
-        /// <param name="status"></param>
+
         public void SetBombsReadyStatus(bool status)
         {
             if (IsBombReady != status)
@@ -169,35 +144,30 @@ namespace Assets.Scripts.Entities.Ships
                 SetIndicatorColor();
             }
         }
-        /// <summary>
-        /// Sets the color of the indicator based on whether the bomb is ready or not, and activates or deactivates the carried bomb GameObject accordingly.
-        /// </summary>
+
         public void SetIndicatorColor()
-        { 
+        {
             if (!Stage.IsTraining)
             {
                 if (IsBombReady)
                 {
                     _indicatorSprite.color = ConfigData.GetUIColor("striker-loaded-indicator");
                     CarriedBomb.SetActive(true);
-
                 }
                 else
                 {
                     _indicatorSprite.color = ConfigData.GetUIColor("striker-not-loaded-indicator");
                     CarriedBomb.SetActive(false);
-
                 }
             }
-
         }
+
         private StrikerBomb _bomb;
         private ScaledTimer _damageTimer = new ScaledTimer();
         private void DropBomb()
         {
             if (!HasDroppedBomb)
             {
-                //Debug.Log($"Striker #{Id} is dropping bombs");
                 HasDroppedBomb = true;
                 SetBombsReadyStatus(false);
 
@@ -209,14 +179,6 @@ namespace Assets.Scripts.Entities.Ships
                 }
                 else
                 {
-                    //if (Level._currentTimerIDs.Contains(_damageTimer.Id)) // [debug]
-                    //{
-                    //    Debug.LogError($"Tried to add {_damageTimer} but it already exists in Timers.");
-                    //}
-                    //else
-                    //{
-                    //    Debug.Log($"Adding {_damageTimer} to Timers.");
-                    //}
                     _damageTimer.Reuse(2, LogBombDamage);
                     Level.AddTimer(_damageTimer);
                 }
@@ -224,33 +186,30 @@ namespace Assets.Scripts.Entities.Ships
                 CompleteRun();
             }
         }
+
         public void LogBombDamage()
         {
             LogAttackingDamage(Bomb.Power, this, FleetShip, Squad.SavedSquad, ContactedShip);
         }
+
         public void CompleteRun()
         {
             HasCompletedRun = true;
             TargetEnemyShipToFollow = null;
             SetIndicatorColor();
-
         }
+
         private Vector2 _destination;
         public void ReturnToCarrierIfNecessary()
         {
-            // If you haven't returned to the carrier and you've either dropped your bombs or don't have them
             if (!HasReturnedToCarrier && (!IsBombReady || HasCompletedRun))
             {
-                // send any bomber that is't loaded to its carrier
-                //Debug.Log($"Sending {striker.Id} back to its carrier");
-                if (!Carrier.IsDead)
+                if (Carrier != null && !Carrier.IsDead)
                 {
                     _destination = Carrier.GetPosition() + OffsetFromCenter;
-                    //Vector2 targetPoint = Level.ForceBounds(destination + OffsetFromCenter);
 
                     if (DistanceToPoint(_destination) < ConfigData.RefillDistanceToCarrier || DistanceTo(Carrier) < ConfigData.RefillDistanceToCarrier)
                     {
-                        //Debug.Log($"{Name} has returned to carrier and is moving towards {destination}");
                         SetBombsReadyStatus(true);
                         if (HasCompletedRun)
                         {
@@ -264,7 +223,6 @@ namespace Assets.Scripts.Entities.Ships
                     }
                     else
                     {
-                        //Debug.Log($"{striker.Id} is still {distance} away from {targetPoint}");
                         MoveToPoint(_destination);
                     }
                 }
@@ -273,7 +231,6 @@ namespace Assets.Scripts.Entities.Ships
 
         public override void Kill(Ship killer, FleetShip killerFleetShip, SavedSquad killerSavedSquad, bool endKill = false)
         {
-            //Debug.Log($"Striker {Name} killed, canceling {_damageTimer}");
             Level.CancelTimer(_damageTimer);
             Level.CancelTimer(_checkCarrierReloadTimer);
             base.Kill(killer, killerFleetShip, killerSavedSquad, endKill);
