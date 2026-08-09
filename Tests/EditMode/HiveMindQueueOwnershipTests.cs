2c6212e337c683a6bef342e6c563b0e43730e5ff
allStackGit <dmoran201@gmail.com>
2026-08-09 12:38:10 -0400

    Cover Hive Mind queue deduplication

---
+++ Tests/EditMode/HiveMindQueueOwnershipTests.cs
@@ -0,0 +1,29 @@
+using System.IO;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Bees.Tests.EditMode
+{
+    [TestFixture]
+    [Category("BeesFoundation")]
+    public class HiveMindQueueOwnershipTests
+    {
+        [Test]
+        public void AwaitingCommandQueueRejectsDeadAndDuplicateSquads()
+        {
+            string path = Path.Combine(Application.dataPath, "Scripts", "Levels", "GameState.Commands.cs");
+            string source = File.ReadAllText(path);
+
+            int methodStart = source.IndexOf("public void AddToSquadsAwaitingHiveMindCommands(Squad squad)");
+            int methodEnd = source.IndexOf("public Queue<Squad> GetSquadsAwaitingHiveMindCommands", methodStart);
+            Assert.That(methodStart, Is.GreaterThanOrEqualTo(0));
+            Assert.That(methodEnd, Is.GreaterThan(methodStart));
+
+            string method = source.Substring(methodStart, methodEnd - methodStart);
+            StringAssert.Contains("squad == null", method);
+            StringAssert.Contains("squad.IsDead", method);
+            StringAssert.Contains("SquadsAwaitingCommands.Contains(squad)", method);
+            StringAssert.Contains("SquadsAwaitingCommands.Enqueue(squad)", method);
+        }
+    }
+}
