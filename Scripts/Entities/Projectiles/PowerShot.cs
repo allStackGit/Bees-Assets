012da058dd0bb1943a214ce8167d59884d77a59c
allStackGit <dmoran201@gmail.com>
2026-08-09 10:23:44 -0400

    Stop power shot mutation after kill

--- Scripts/Entities/Projectiles/PowerShot.cs
+++ Scripts/Entities/Projectiles/PowerShot.cs
@@ -1,5 +1,4 @@
 ﻿using Assets.Scripts.Entities.Ships;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,28 +8,30 @@ namespace Assets.Scripts.Entities.Projectiles
     {
         private int _powerLoss;
         private HashSet<Ship> _shipsHit = new HashSet<Ship>();
+
         public override void ClearData()
         {
             base.ClearData();
             _shipsHit.Clear();
         }
+
         public override void ContactTarget(Ship target)
         {
-            if (!_shipsHit.Contains(target))
+            if (_shipsHit.Contains(target))
             {
-                _shipsHit.Add(target);
-                //Debug.Log($"{Name} hit {target.Name}");
-                _powerLoss = Mathf.Clamp(target.Health, 0, Power);
-                if (Power <= target.Health)
-                {
-                    KillSequence();
-                }
-                Power -= _powerLoss;
-                //Debug.Log($"{Name} lost {_powerLoss} power and now has {Power} power.");
-                _powerLoss = 0;
+                return;
             }
-            
 
+            _shipsHit.Add(target);
+            _powerLoss = Mathf.Clamp(target.Health, 0, Power);
+            if (Power <= target.Health)
+            {
+                KillSequence();
+                return;
+            }
+
+            Power -= _powerLoss;
+            _powerLoss = 0;
         }
     }
 }
