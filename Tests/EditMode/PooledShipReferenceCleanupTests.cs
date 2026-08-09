9f58b72847b54581acddc8677bca482313fecc21
allStackGit <dmoran201@gmail.com>
2026-08-09 12:33:02 -0400

    Cover pooled ship reference cleanup

---
+++ Tests/EditMode/PooledShipReferenceCleanupTests.cs
@@ -0,0 +1,34 @@
+using System.IO;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Bees.Tests.EditMode
+{
+    [TestFixture]
+    [Category("BeesFoundation")]
+    public class PooledShipReferenceCleanupTests
+    {
+        [Test]
+        public void RemoveShipClearsDamageAndSpottingReferencesBeforeRelease()
+        {
+            string path = Path.Combine(Application.dataPath, "Scripts", "Levels", "GameState.Registry.cs");
+            string source = File.ReadAllText(path);
+
+            int methodStart = source.IndexOf("public void RemoveShip(Ship ship)");
+            int methodEnd = source.IndexOf("public void AddDeadBody", methodStart);
+            Assert.That(methodStart, Is.GreaterThanOrEqualTo(0));
+            Assert.That(methodEnd, Is.GreaterThan(methodStart));
+
+            string method = source.Substring(methodStart, methodEnd - methodStart);
+            int damageCleanup = method.IndexOf("statuses.RemoveAll");
+            int spottedCleanup = method.IndexOf("spotted.RemoveAll");
+            int releaseQueue = method.IndexOf("ShipsToRelease.Add(ship)");
+
+            Assert.That(damageCleanup, Is.GreaterThanOrEqualTo(0));
+            Assert.That(spottedCleanup, Is.GreaterThan(damageCleanup));
+            Assert.That(releaseQueue, Is.GreaterThan(spottedCleanup));
+            StringAssert.Contains("status.Ship == ship", method);
+            StringAssert.Contains("entry.Ship == ship", method);
+        }
+    }
+}
