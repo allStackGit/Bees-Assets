efafbd9711c5476bdea938a9c72f9ebb9a7013eb
allStackGit <dmoran201@gmail.com>
2026-08-09 09:00:01 -0400

    Split Ship input and collider interaction

---
+++ Scripts/Entities/Ships/Ship.Interaction.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+namespace Assets.Scripts.Entities.Ships
+{
+    public partial class Ship
+    {
+        protected virtual void OnTriggerEnter2D(Collider2D collider)
+        {
+            _tempCollidingThing = collider.gameObject;
+            if (_tempCollidingThing.name == "Selection Box" && IsUserControlled)
+            {
+                Stage.Selector.SelectShip(this);
+            }
+        }
+
+        protected virtual void OnTriggerExit2D(Collider2D collider)
+        {
+            _tempCollidingThing = collider.gameObject;
+            if (_tempCollidingThing.name == "Selection Box" && IsUserControlled)
+            {
+                Stage.Selector.DeselectShip(this);
+            }
+        }
+
+        public void Clicked(int mouseButton, bool isCtrlClick = false)
+        {
+            if (!IsUserControlled && mouseButton == LevelInputManager.RightClick)
+            {
+                Level.State.GetSelectedSquads().ForEach(selectedSquad => selectedSquad.UserAggressive(Squad));
+            }
+            else if (IsUserControlled && mouseButton == LevelInputManager.LeftClick && !Squad.IsImmobile)
+            {
+                if (isCtrlClick) Level.State.AddSelectedSquad(Squad);
+                else Level.State.SelectSquad(Squad);
+            }
+        }
+    }
+}
