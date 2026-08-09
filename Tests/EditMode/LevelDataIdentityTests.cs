9154428d26ccd3b1838eeee6bb1661225f2a4506
allStackGit <dmoran201@gmail.com>
2026-08-09 00:01:15 -0400

    Cover sparse persisted level ID allocation

--- Tests/EditMode/LevelDataIdentityTests.cs
+++ Tests/EditMode/LevelDataIdentityTests.cs
@@ -14,23 +14,40 @@ namespace Bees.Tests.EditMode
         [Test]
         public void GetLevelUsesPersistedIdInsteadOfListPosition()
         {
+            object levelData = CreateLevelDataWithIds(42, 7);
+
+            object selected = RuntimeAssembly.Invoke(levelData, "GetLevel", 7);
+
+            Assert.That(RuntimeAssembly.GetField(selected, "Id"), Is.EqualTo(7));
+            Assert.That(RuntimeAssembly.GetField(selected, "Name"), Is.EqualTo("Level 7"));
+        }
+
+        [Test]
+        public void GetNewIdUsesHighestPersistedIdInsteadOfCount()
+        {
+            object levelData = CreateLevelDataWithIds(42, 7);
+
+            Assert.That(RuntimeAssembly.Invoke(levelData, "GetCurrentId"), Is.EqualTo(42));
+            Assert.That(RuntimeAssembly.Invoke(levelData, "GetNewId"), Is.EqualTo(43));
+        }
+
+        private static object CreateLevelDataWithIds(params int[] ids)
+        {
             Type levelDataType = RuntimeAssembly.GetType("Assets.Scripts.Data.LevelData");
             Type levelOptionsType = RuntimeAssembly.GetType("Assets.Scripts.Data.LevelOptions");
             object levelData = FormatterServices.GetUninitializedObject(levelDataType);
 
             Type listType = typeof(List<>).MakeGenericType(levelOptionsType);
             IList levels = (IList)Activator.CreateInstance(listType);
-            levels.Add(Activator.CreateInstance(levelOptionsType, new object[] { 42, 1, "Forty Two" }));
-            levels.Add(Activator.CreateInstance(levelOptionsType, new object[] { 7, 1, "Seven" }));
+            foreach (int id in ids)
+            {
+                levels.Add(Activator.CreateInstance(levelOptionsType, new object[] { id, 1, $"Level {id}" }));
+            }
 
             FieldInfo levelsField = levelDataType.GetField("_levels", BindingFlags.Instance | BindingFlags.NonPublic);
             Assert.That(levelsField, Is.Not.Null);
             levelsField.SetValue(levelData, levels);
-
-            object selected = RuntimeAssembly.Invoke(levelData, "GetLevel", 7);
-
-            Assert.That(RuntimeAssembly.GetField(selected, "Id"), Is.EqualTo(7));
-            Assert.That(RuntimeAssembly.GetField(selected, "Name"), Is.EqualTo("Seven"));
+            return levelData;
         }
     }
 }
