0112b7a0572e7bf741a97fb28728a90032d7fc9f
allStackGit <dmoran201@gmail.com>
2026-08-09 09:58:25 -0400

    Release laser beam shooter ownership on kill

--- Scripts/Entities/Projectiles/LaserBeam.cs
+++ Scripts/Entities/Projectiles/LaserBeam.cs
@@ -10,10 +10,7 @@ namespace Assets.Scripts.Entities.Projectiles
 {
     public class LaserBeam : Projectile
     {
-
-        //private Vector2 _lastTargetPoint;
         private float _scale = 2f;
-        //private Ship _target;
         private int _powerLoss;
         private HashSet<Ship> _shipsHit = new HashSet<Ship>();
         private Vector2 _oneAndHalf = new Vector2(1, .5f);
@@ -22,21 +19,12 @@ namespace Assets.Scripts.Entities.Projectiles
         {
             base.Setup(level, beamCannon, shooter, target, startingPosition, angle, range, power);
             BeamCannon = beamCannon;
-            //_lastShooterPosition = Weapon.GetPosition();
-            //_target = Weapon.TargetShip;
-            //if (_target != null)
-            //{
-            //    _lastTargetPoint = _target.GetPosition();
-            //}
             _halfRange = Range / 2;
         }
 
         public override void ClearData()
         {
             base.ClearData();
-            //_lastShooterPosition = Vector2.zero;
-            //_lastTargetPoint = Vector2.zero;
-            //_target = null;
             _powerLoss = 0;
             Transform.localScale = _oneAndHalf;
             _shipsHit.Clear();
@@ -44,16 +32,11 @@ namespace Assets.Scripts.Entities.Projectiles
         }
         private int _halfHealth;
 
-        /// <summary>
-        /// If the target hasn't been hit before, reduces the damage of the laser beam by half the health of the target ship and continues on if it still has power
-        /// </summary>
-        /// <param name="target"></param>
         public override void ContactTarget(Ship target)
         {
             if (!_shipsHit.Contains(target))
             {
                 _shipsHit.Add(target);
-                //Debug.Log($"{Name} hit {target.Name}");
                 _halfHealth = target.Health / 2;
                 _powerLoss = Mathf.Clamp(_halfHealth, 0, Power);
                 if (Power <= _halfHealth)
@@ -61,12 +44,8 @@ namespace Assets.Scripts.Entities.Projectiles
                     Kill();
                 }
                 Power -= _powerLoss;
-                //Debug.Log($"{Name} lost {_powerLoss} power and now has {Power} power.");
                 _powerLoss = 0;
             }
-
-
-
         }
         public override void Kill()
         {
@@ -77,24 +56,12 @@ namespace Assets.Scripts.Entities.Projectiles
                 BeamCannon.LaserBeamTarget = null;
                 BeamCannon.StopSoundEffect();
                 RemoveDamageSentEntry();
-                if (!ShipIsDead)
-                {
-                    Shooter.ProjectilesInFlight.Remove(this);
-                }
-                //Debug.Log($"{Name} has been killed and will be returned");
+                Shooter?.ProjectilesInFlight.Remove(this);
                 Level.State.RemoveProjectile(this);
                 Deactivate();
                 Stage.Pool.ReturnProjectileToPool(this);
             }
-
         }
-        //private Vector2 GetChangeInShooterPosition()
-        //{
-        //    Vector2 position = Shooter.GetPosition();
-        //    Vector2 change = position - _lastShooterPosition;
-        //    _lastShooterPosition = position;
-        //    return change;
-        //}
         protected override void FixedUpdate()
         {
             base.FixedUpdate();
@@ -108,67 +75,28 @@ namespace Assets.Scripts.Entities.Projectiles
         private Vector3 _forward = Vector3.forward;
         private void ExtendBeam()
         {
-
             if (!Shooter.IsDead)
             {
-                //Debug.Log($"Extending beam towards {_target.Name}");
-                //if (_target != null)
-                //{
-                //    _lastTargetPoint = _target.GetPosition();
-                //}
-                //float distance = Weapon.DistanceToPoint(_lastTargetPoint);
-
                 if (Transform.localScale.x < _halfRange)
                 {
-                    //if (_scale == 0)
-                    //{
-                    //    transform.localScale = new Vector2(Weapon.DistanceToPoint(_lastTargetPoint) / 2, transform.localScale.y);
-                    //}
                     Transform.localScale = new Vector2(Transform.localScale.x + _scale, Transform.localScale.y);
 
                     _worldAngle = BeamCannon.Rotation + 90;
                     _localAngle = BeamCannon.GetLocalRotation() + 90;
 
                     _laserBeamOffset = new Vector2(Transform.localScale.x, 0);
-
                     _localRotation = Quaternion.Euler(0, 0, _localAngle);
-
                     _localCannonPoint = Vector2.zero + _laserBeamOffset;
-                    //Debug.Log($"Beamcannon local position: {Weapon.GetLocalPosition()}");
                     _rotatedLocalPosition = _localRotation * _localCannonPoint;
-                    //Vector2 rotatedCannonPosition = (Vector2)Shooter.transform.TransformPoint(rotatedLocalPosition);
-                    //Vector2 rotatedMapPosition = (Vector2)Level.Map.Transform.TransformPoint(rotatedLocalPosition);
                     _offsetRotatedCannonPosition = (Vector2)Shooter.transform.TransformPoint(_rotatedLocalPosition) - Level.GetPosition();
                     Angle = _worldAngle * -Mathf.Deg2Rad;
-
-                    //Debug.Log($"Cruiser world rotation: {Shooter.transform.eulerAngles.z}");
-                    //Debug.Log($"Cannon local rotation: {Weapon.GetLocalRotation()}, world rotation: {Weapon.GetRotation()}");
-                    //Debug.Log($"Beam world rotation: {worldAngle}");
-
-                    //Debug.Log($"localCannonPoint #{Id}: {localCannonPoint}");
-                    //Debug.Log($"rotatedLocalPosition #{Id}: {rotatedLocalPosition}");
-                    //Debug.Log($"rotatedMapPosition #{Id}: {rotatedMapPosition}");
-
-                    //Debug.Log($"offsetRotatedCannonPosition #{Id}: {offsetRotatedCannonPosition}");
-
-                    //Debug.Log($"Extending Laser Beam #{Id} to rotated cannon position: {offsetRotatedCannonPosition}");
 
                     Transform.localEulerAngles = _forward * _worldAngle;
                     Transform.localPosition = _offsetRotatedCannonPosition;
                     Body.linearVelocity = Shooter.Body.linearVelocity;
                     return;
-
-                    //+ GetChangeInShooterPosition();
                 }
-                //else
-                //{
-                //    //Debug.Log($"Killing {gameObject.name} at position ({transform.localPosition}) because distance ({distance}) > range ({Range})");
-                //}
             }
-            //else
-            //{
-            //    //Debug.Log($"Killing {gameObject.name} at position ({transform.localPosition}) because the shooter is dead");
-            //}
             if (!BeamCannon.IsFiringManually)
             {
                 RemoveDamageSentEntry();
