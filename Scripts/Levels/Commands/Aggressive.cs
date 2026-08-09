a9bb6542ebd0a869b97d1b780eeb04405ba8cce3
allStackGit <dmoran201@gmail.com>
2026-08-09 10:01:55 -0400

    Defer invalid queued aggressive finalization

--- Scripts/Levels/Commands/Aggressive.cs
+++ Scripts/Levels/Commands/Aggressive.cs
@@ -17,15 +17,19 @@ namespace Assets.Scripts.Levels.Commands
         public bool HasTakenPosition;
         public int ConsecutiveTimesWithinRange = 0;
 
-        /// <summary>
-        ///  Sends the squad towards the enemy and follows them, attacking until one squad is dead
-        /// </summary>
-        /// <param name="strategy"></param>
-        /// <param name="shootingStrategy"></param>
-        /// <param name="commandOutcomeId"></param>
-        /// <param name="noEnemy"></param>
         public void Execute(ConfigData.ShootingStrategyTypes shootingStrategy, long commandOutcomeId, long shootingStrategyOutcomeId)
         {
+            // Command.Setup marks the squad as having this command before Execute runs.
+            // A queued Aggressive command without a valid enemy must not finalize
+            // synchronously here, because Squad.RunCommandQueue historically reasserts
+            // HasCommand after Execute returns. Finalize on the next timer update instead.
+            if (!HasEnemy)
+            {
+                CommandTimer.Reuse(0f, () => SetFinalize("Could not find the enemy squad for command"));
+                Level.AddTimer(CommandTimer);
+                return;
+            }
+
             base.Execute(shootingStrategy, commandOutcomeId, shootingStrategyOutcomeId, false);
             if (IsDead)
             {
@@ -45,7 +49,6 @@ namespace Assets.Scripts.Levels.Commands
                     Level.AddTimer(TimeoutTimer);
                 }
             }
-            
         }
         public override void ClearData()
         {
@@ -61,44 +64,34 @@ namespace Assets.Scripts.Levels.Commands
                 if (!EnemySquad.IsDead)
                 {
                     GetSquad().Status = $"Targeting enemy squad #{EnemySquad.SquadNumber}";
-                    if (!IsComfortablyWithinRange) // check if all of their squad ships are comfortably within range of all of our squad ships
+                    if (!IsComfortablyWithinRange)
                     {
                         if (GetSquad().AreSomeSquadShipsWithinRangeOfAllOfOurSquadShips(EnemySquad))
                         {
-                            // Keep track of how many times all of the ships have been within range so we don't stop moving towards the enemy until we've been within range for a little bit at least
                             ConsecutiveTimesWithinRange++;
                             if (ConsecutiveTimesWithinRange == 3)
                             {
                                 ConsecutiveTimesWithinRange = 0;
                                 IsComfortablyWithinRange = true;
-                                //Debug.Log($"We are comfortably within range");
                             }
-                            //Debug.Log($"All ships are within range of the some ships in the enemy squad, we can stop moving towards them? {ConsecutiveTimesWithinRange}");
                         }
                         else
                         {
                             ConsecutiveTimesWithinRange = 0;
                         }
-                        //Debug.Log($"Enemy: {Enemy.Name} IsDead: {Enemy.IsDead}");
-                        //SetAndMove(Enemy.GetPosition());
                         MoveTowardsEnemies();
 
-                        // Once we get close to the target we speed up the timer so we get more up to date information
                         if (!IsCloseToTarget && GetSquad().DistanceToPoint(EnemySquad.GetPosition()) < GetSquad().MaxRange * 2)
                         {
-                            //Debug.Log($"{GetSquad().Name} is close to {EnemySquad.Name}");
                             Level.CancelTimer(CommandTimer);
-                            //CancelInvoke(nameof(Timer));
                             CommandFrequency = .25f;
                             IsCloseToTarget = true;
                             CommandTimer.Reuse(CommandFrequency, Timer, true);
                             Level.AddTimer(CommandTimer);
-                            //InvokeRepeating(nameof(Timer), CommandFrequency, CommandFrequency);
                         }
                     }
                     else if ((GetSquad().MaxRange >= 45 && GetSquad().AreAllSquadShipsWithinRangeOfAllOfOurSquadShips(EnemySquad)) || (GetSquad().AreAllSquadShipsWithinRangeOfAllOfOurSquadShips(EnemySquad) && EnemySquad.IsDefenseless))
                     {
-                        //Debug.Log($"All ships are comfortably within range, we don't need to move."); 
                         if (!HasTakenPosition)
                         {
                             SetAndMove(GetSquad().GetPosition());
@@ -113,13 +106,9 @@ namespace Assets.Scripts.Levels.Commands
                 }
                 else
                 {
-                    //CancelInvoke(nameof(Timer));
                     SetFinalize("The enemy squad is gone or dead");
                 }
             }
-            
         }
-
-        
     }
 }
