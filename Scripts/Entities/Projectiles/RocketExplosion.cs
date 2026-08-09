198a3dba359d19bc8c00d4e628940fe50c7ca166
allStackGit <dmoran201@gmail.com>
2026-08-09 09:29:43 -0400

    Drain all queued explosion contacts

--- Scripts/Entities/Projectiles/RocketExplosion.cs
+++ Scripts/Entities/Projectiles/RocketExplosion.cs
@@ -1,23 +1,16 @@
-﻿
-using Assets.Scripts.Entities.Ships;
-using System.Collections;
+﻿using Assets.Scripts.Entities.Ships;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 namespace Assets.Scripts.Entities.Projectiles
 {
-
-    // Explosions don't move but they damage all targets that they touch
-    // They operate like normal projectiles upon contact except that contact doesn't kill them and since they linger they can only damage a target once
+    // Explosions don't move but they damage all targets that they touch. They operate like
+    // normal projectiles except that contact does not kill them and each target is damaged once.
     public class RocketExplosion : Projectile
     {
-        /// <summary>
-        ///  After a few frames, the explosion is no longer damaging and becomes harmless to whoever hits it
-        /// </summary>
         public bool IsHarmless;
         public List<Ship> _shipsHit = new List<Ship>();
-        private HashSet<Obstacle> _obstaclesHit = new HashSet<Obstacle>();
+        private readonly HashSet<Obstacle> _obstaclesHit = new HashSet<Obstacle>();
         public CircleCollider2D CircleCollider;
 
         public override void ClearData()
@@ -27,35 +20,35 @@ namespace Assets.Scripts.Entities.Projectiles
             _obstaclesHit.Clear();
             IsHarmless = false;
         }
+
         public override void Activate()
         {
             base.Activate();
             CircleCollider.enabled = true;
         }
+
         public override void Deactivate()
         {
             base.Deactivate();
             CircleCollider.enabled = false;
         }
+
         public override void ContactTarget(Ship target)
         {
-            //Debug.Log($"Explosion {Name} hit {target.Name}");
             _shipsHit.Add(target);
         }
 
         public override void ContactObstacle(Obstacle obstacle)
         {
-           if (!obstacle.IsDead)
+            if (obstacle.IsDead || obstacle.ObstacleType == ConfigData.ObstacleTypes.MapBorder || HasHitObstacle(obstacle))
             {
-                if (obstacle.ObstacleType != ConfigData.ObstacleTypes.MapBorder && !HasHitObstacle(obstacle))
-                {
-                    //Debug.Log($"{Name} hit {obstacle.Name}");
-                    if (obstacle.ObstacleType == ConfigData.ObstacleTypes.CollisionAsteroid)
-                    {
-                        DamageObstacle((CollisionAsteroid)obstacle);
-                    }
-                    //DamageObstacle(obstacle);
-                }
+                return;
+            }
+
+            _obstaclesHit.Add(obstacle);
+            if (obstacle.ObstacleType == ConfigData.ObstacleTypes.CollisionAsteroid)
+            {
+                DamageObstacle((CollisionAsteroid)obstacle);
             }
         }
 
@@ -71,7 +64,6 @@ namespace Assets.Scripts.Entities.Projectiles
 
         public override void Kill()
         {
-            //Debug.Log("Killed off the rocket explosion");
             if (Type == ConfigData.ProjectileTypes.FireBargeExplosion)
             {
                 Level.State.FireBargeExplosions.Remove(this);
@@ -82,8 +74,8 @@ namespace Assets.Scripts.Entities.Projectiles
         public void SetHarmless()
         {
             IsHarmless = true;
-            //Debug.Log($"{Name} is now harmless");
         }
+
         public override void RemoveDamageSentEntry()
         {
             if (Target != null)
@@ -91,23 +83,21 @@ namespace Assets.Scripts.Entities.Projectiles
                 base.RemoveDamageSentEntry();
             }
         }
+
         public void SetColliderSize(int size)
         {
-            //Debug.Log($"Setting collider size to {size} for {Name}");
             CircleCollider.radius = size;
         }
-        private int _index;
+
         protected override void FixedUpdate()
         {
-            if (CollidingQueue.Count > 0)
+            // Count changes on Dequeue, so a for-loop bounded by the live Count skips roughly
+            // half of simultaneous contacts. Drain the queues instead.
+            while (CollidingQueue.Count > 0)
             {
-                //Debug.Log($"Pulled collision for {Name} off of rocket explosion queue");
-                for (_index = 0; _index < CollidingQueue.Count; _index++)
-                {
-                    ShipCollision(CollidingQueue.Dequeue());
-                }
+                ShipCollision(CollidingQueue.Dequeue());
             }
-            if (CollidingObstacleQueue.Count > 0)
+            while (CollidingObstacleQueue.Count > 0)
             {
                 ContactObstacle(CollidingObstacleQueue.Dequeue());
             }
@@ -115,7 +105,6 @@ namespace Assets.Scripts.Entities.Projectiles
 
         protected override void ShipCollision(Ship ship)
         {
-            //Debug.Log($"Rocket explosion {Name} collided with {ship.Name}");
             if (ProjectileDamagePolicy.CanExplosionDamage(
                 Shooter.Side,
                 Shooter.ShipType,
@@ -129,10 +118,5 @@ namespace Assets.Scripts.Entities.Projectiles
                 Ship.LogAttackingDamage(Power, Shooter, FleetShip, SavedSquad, ship);
             }
         }
-
     }
-
-
-
-
 }
