9ad47c47f2808dfaaec96e19c1743721a8fafb84
allStackGit <dmoran201@gmail.com>
2026-08-09 09:45:06 -0400

    Stop finalized aggressive commands before scheduling timers

--- Scripts/Levels/Commands/Aggressive.cs
+++ Scripts/Levels/Commands/Aggressive.cs
@@ -27,6 +27,10 @@ namespace Assets.Scripts.Levels.Commands
         public void Execute(ConfigData.ShootingStrategyTypes shootingStrategy, long commandOutcomeId, long shootingStrategyOutcomeId)
         {
             base.Execute(shootingStrategy, commandOutcomeId, shootingStrategyOutcomeId, false);
+            if (IsDead)
+            {
+                return;
+            }
 
             if (!GetSquad().IsDead)
             {
@@ -57,44 +61,34 @@ namespace Assets.Scripts.Levels.Commands
                 if (!EnemySquad.IsDead)
                 {
                     GetSquad().Status = $"Targeting enemy squad #{EnemySquad.SquadNumber}";
-                    if (!IsComfortablyWithinRange) // check if all of their squad ships are comfortably within range of all of our squad ships
+                    if (!IsComfortablyWithinRange)
                     {
                         if (GetSquad().AreSomeSquadShipsWithinRangeOfAllOfOurSquadShips(EnemySquad))
                         {
-                            // Keep track of how many times all of the ships have been within range so we don't stop moving towards the enemy until we've been within range for a little bit at least
                             ConsecutiveTimesWithinRange++;
                             if (ConsecutiveTimesWithinRange == 3)
                             {
                                 ConsecutiveTimesWithinRange = 0;
                                 IsComfortablyWithinRange = true;
-                                //Debug.Log($"We are comfortably within range");
                             }
-                            //Debug.Log($"All ships are within range of the some ships in the enemy squad, we can stop moving towards them? {ConsecutiveTimesWithinRange}");
                         }
                         else
                         {
                             ConsecutiveTimesWithinRange = 0;
                         }
-                        //Debug.Log($"Enemy: {Enemy.Name} IsDead: {Enemy.IsDead}");
-                        //SetAndMove(Enemy.GetPosition());
                         MoveTowardsEnemies();
 
-                        // Once we get close to the target we speed up the timer so we get more up to date information
                         if (!IsCloseToTarget && GetSquad().DistanceToPoint(EnemySquad.GetPosition()) < GetSquad().MaxRange * 2)
                         {
-                            //Debug.Log($"{GetSquad().Name} is close to {EnemySquad.Name}");
                             Level.CancelTimer(CommandTimer);
-                            //CancelInvoke(nameof(Timer));
                             CommandFrequency = .25f;
                             IsCloseToTarget = true;
                             CommandTimer.Reuse(CommandFrequency, Timer, true);
                             Level.AddTimer(CommandTimer);
-                            //InvokeRepeating(nameof(Timer), CommandFrequency, CommandFrequency);
                         }
                     }
                     else if ((GetSquad().MaxRange >= 45 && GetSquad().AreAllSquadShipsWithinRangeOfAllOfOurSquadShips(EnemySquad)) || (GetSquad().AreAllSquadShipsWithinRangeOfAllOfOurSquadShips(EnemySquad) && EnemySquad.IsDefenseless))
                     {
-                        //Debug.Log($"All ships are comfortably within range, we don't need to move."); 
                         if (!HasTakenPosition)
                         {
                             SetAndMove(GetSquad().GetPosition());
@@ -109,13 +103,10 @@ namespace Assets.Scripts.Levels.Commands
                 }
                 else
                 {
-                    //CancelInvoke(nameof(Timer));
                     SetFinalize("The enemy squad is gone or dead");
                 }
             }
             
         }
-
-        
     }
 }
