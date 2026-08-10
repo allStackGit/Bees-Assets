f4bc20a7ebd5a5aa76effee868e79f4705601539
allStackGit <dmoran201@gmail.com>
2026-08-09 22:01:00 -0400

    Make campaign resource test line-ending agnostic

--- Tests/EditMode/CampaignResourceAccountingTests.cs
+++ Tests/EditMode/CampaignResourceAccountingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using NUnit.Framework;
 using UnityEngine;
@@ -31,8 +32,43 @@ namespace Bees.Tests.EditMode
             string path = Path.Combine(Application.dataPath, "Scripts", "Levels", "Level.Campaign.Endings.cs");
             string source = File.ReadAllText(path);
 
-            StringAssert.Contains("else\n            {\n                ConfigData.UserProgressData.AdvanceToNextLevel();\n                int mineralsMined = 0;", source);
-            StringAssert.Contains("WinningSide == ConfigData.Configuration.AISide || !ConfigData.CurrentShips.HasShipsOfType(ConfigData.ShipTypes.Factory)", source);
+            string neptune1 = ExtractMethodBody(source, "Neptune1Ending");
+            int lossAdvance = neptune1.IndexOf("ConfigData.UserProgressData.AdvanceToNextLevel();", StringComparison.Ordinal);
+            int mineralsAccounting = neptune1.IndexOf("int mineralsMined = 0;", StringComparison.Ordinal);
+            int completionAdvance = neptune1.IndexOf(
+                "ConfigData.UserProgressData.AdvanceToNextLevel();",
+                lossAdvance + 1,
+                StringComparison.Ordinal);
+
+            Assert.That(neptune1, Does.Contain("WinningSide == ConfigData.Configuration.UserSide"));
+            Assert.That(lossAdvance, Is.GreaterThanOrEqualTo(0));
+            Assert.That(mineralsAccounting, Is.GreaterThan(lossAdvance));
+            Assert.That(completionAdvance, Is.GreaterThan(mineralsAccounting));
+
+            string uranus1 = ExtractMethodBody(source, "Uranus1Ending");
+            Assert.That(uranus1, Does.Contain(
+                "WinningSide == ConfigData.Configuration.AISide || !ConfigData.CurrentShips.HasShipsOfType(ConfigData.ShipTypes.Factory)"));
+        }
+
+        private static string ExtractMethodBody(string source, string methodName)
+        {
+            int signature = source.IndexOf(" " + methodName + "(", StringComparison.Ordinal);
+            Assert.That(signature, Is.GreaterThanOrEqualTo(0), $"Could not find method {methodName}.");
+            int openingBrace = source.IndexOf('{', signature);
+            Assert.That(openingBrace, Is.GreaterThanOrEqualTo(0));
+
+            int depth = 0;
+            for (int index = openingBrace; index < source.Length; index++)
+            {
+                if (source[index] == '{') depth++;
+                else if (source[index] == '}' && --depth == 0)
+                {
+                    return source.Substring(openingBrace, index - openingBrace + 1);
+                }
+            }
+
+            Assert.Fail($"Method {methodName} has no balanced body.");
+            return string.Empty;
         }
     }
 }
