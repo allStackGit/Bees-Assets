e14bd43d1df4e38c78a79eaecd50acd74e41d285
allStackGit <dmoran201@gmail.com>
2026-08-09 20:15:02 -0400

    Cover Guard cleanup on all finalization paths

---
+++ Tests/EditMode/GuardCommandLifecycleTests.cs
@@ -0,0 +1,23 @@
+using System.IO;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Bees.Tests.EditMode
+{
+    [TestFixture]
+    [Category("BeesFoundation")]
+    public class GuardCommandLifecycleTests
+    {
+        [Test]
+        public void GuardFinalizationAlwaysRestoresSpeedAndReciprocalMembership()
+        {
+            string path = Path.Combine(Application.dataPath, "Scripts", "Levels", "Commands", "Guard.cs");
+            string source = File.ReadAllText(path);
+
+            StringAssert.Contains("public override void SetFinalize(string cause)", source);
+            StringAssert.Contains("GetSquad().SetSquadSpeed(GetSquad().MaxSpeed);", source);
+            StringAssert.Contains("((Guard)guardSquad.GetCommand()).OtherGuardSquads.Remove(GetSquad());", source);
+            StringAssert.Contains("base.SetFinalize(cause);", source);
+        }
+    }
+}
