52848c589a0ce04a76111562d6aef7d42682325b
allStackGit <dmoran201@gmail.com>
2026-08-10 08:00:31 -0400

    Cover level-local movement marker placement

--- Tests/EditMode/MultiLevelShipPhysicsTests.cs
+++ Tests/EditMode/MultiLevelShipPhysicsTests.cs
@@ -19,5 +19,17 @@ namespace Bees.Tests.EditMode
             Assert.That(body, Does.Contain("GetPosition() + Level.GetPosition()"));
             Assert.That(body, Does.Not.Contain("GetLiveObstacleFromBoxCast(\n                GetPosition(),"));
         }
+
+        [Test]
+        public void MovementMarkerUsesItsLevelLocalDestinationUnderMapParent()
+        {
+            string source = File.ReadAllText(Path.Combine(Application.dataPath, "Scripts", "Entities", "Ships", "Ship.Movement.cs"));
+            int method = source.IndexOf("private void MoveMovementMarker()");
+            int nextMethod = source.IndexOf("public void SetTargetCoordinates", method);
+            string body = source.Substring(method, nextMethod - method);
+
+            Assert.That(body, Does.Contain("MovementMarker.transform.localPosition = FinalDestination"));
+            Assert.That(body, Does.Not.Contain("MovementMarker.transform.position = FinalDestination"));
+        }
     }
 }
