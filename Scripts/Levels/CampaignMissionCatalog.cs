620a5a1f87685d77c54ab65c25e8d476e85e68a8
allStackGit <dmoran201@gmail.com>
2026-08-10 19:36:40 -0400

    Make campaign map ownership explicit

--- Scripts/Levels/CampaignMissionCatalog.cs
+++ Scripts/Levels/CampaignMissionCatalog.cs
@@ -20,6 +20,7 @@ namespace Assets.Scripts.Levels
         {
             public readonly int Id;
             public readonly string Name;
+            public readonly int MapIndex;
             public readonly string SetupMethod;
             public readonly string CompletionMethod;
             public readonly string TerminalMethod;
@@ -27,13 +28,14 @@ namespace Assets.Scripts.Levels
             public readonly AutomatedScenarioStatus ScenarioStatus;
             private readonly Action<Level> _configure;
 
-            public MissionDefinition(int id, string name, string setupMethod, string completionMethod,
+            public MissionDefinition(int id, string name, int mapIndex, string setupMethod, string completionMethod,
                 string terminalMethod, Action<Level> configure,
                 bool hasPersistedLevelData = true,
                 AutomatedScenarioStatus scenarioStatus = AutomatedScenarioStatus.Ready)
             {
                 Id = id;
                 Name = name;
+                MapIndex = mapIndex;
                 SetupMethod = setupMethod;
                 CompletionMethod = completionMethod;
                 TerminalMethod = terminalMethod;
@@ -51,21 +53,21 @@ namespace Assets.Scripts.Levels
         public static readonly IReadOnlyList<MissionDefinition> Definitions =
             new MissionDefinition[]
             {
-                new MissionDefinition(0, "Anomaly", nameof(Level.Pluto1Anomaly), nameof(Level.Pluto1Ending), nameof(Level.Pluto1Ending), level => level.Pluto1Anomaly()),
-                new MissionDefinition(1, "Reinforcements", nameof(Level.Pluto2Reinforcements), nameof(Level.Pluto2Ending), nameof(Level.Pluto2Ending), level => level.Pluto2Reinforcements()),
-                new MissionDefinition(2, "Pushback", nameof(Level.Pluto3Pushback), nameof(Level.Pluto3Ending), nameof(Level.Pluto3Ending), level => level.Pluto3Pushback()),
-                new MissionDefinition(3, "Bluer Pastures", nameof(Level.Pluto4BluerPasturesCampaign), nameof(Level.Pluto4Ending), nameof(Level.Pluto4EndingDialogue), level => level.Pluto4BluerPasturesCampaign()),
-                new MissionDefinition(4, "Seize the Means", nameof(Level.Neptune1SeizeTheMeans), nameof(Level.Neptune1Ending), nameof(Level.Neptune1Ending), level => level.Neptune1SeizeTheMeans()),
-                new MissionDefinition(5, "Of Production", nameof(Level.Neptune2OfProduction), nameof(Level.Neptune2Ending), nameof(Level.Neptune2Ending), level => level.Neptune2OfProduction()),
-                new MissionDefinition(6, "Pressing Forward", nameof(Level.Neptune3PressingForwardCampaign), nameof(Level.Neptune3Ending), nameof(Level.Neptune3Ending), level => level.Neptune3PressingForwardCampaign()),
-                new MissionDefinition(7, "Minesweeper", nameof(Level.Titania1MinesweeperCampaign), nameof(Level.Titania1MinesweeperEnding), nameof(Level.Titania1MinesweeperEnding), level => level.Titania1MinesweeperCampaign(), true, AutomatedScenarioStatus.InDevelopment),
-                new MissionDefinition(8, "Beenoculars", nameof(Level.Titania2BeenocularsCampaign), nameof(Level.Titania2CampaignEnding), nameof(Level.Titania2CampaignEnding), level => level.Titania2BeenocularsCampaign(), true, AutomatedScenarioStatus.InDevelopment),
+                new MissionDefinition(0, "Anomaly", 0, nameof(Level.Pluto1Anomaly), nameof(Level.Pluto1Ending), nameof(Level.Pluto1Ending), level => level.Pluto1Anomaly()),
+                new MissionDefinition(1, "Reinforcements", 0, nameof(Level.Pluto2Reinforcements), nameof(Level.Pluto2Ending), nameof(Level.Pluto2Ending), level => level.Pluto2Reinforcements()),
+                new MissionDefinition(2, "Pushback", 0, nameof(Level.Pluto3Pushback), nameof(Level.Pluto3Ending), nameof(Level.Pluto3Ending), level => level.Pluto3Pushback()),
+                new MissionDefinition(3, "Bluer Pastures", 0, nameof(Level.Pluto4BluerPasturesCampaign), nameof(Level.Pluto4Ending), nameof(Level.Pluto4EndingDialogue), level => level.Pluto4BluerPasturesCampaign()),
+                new MissionDefinition(4, "Seize the Means", 1, nameof(Level.Neptune1SeizeTheMeans), nameof(Level.Neptune1Ending), nameof(Level.Neptune1Ending), level => level.Neptune1SeizeTheMeans()),
+                new MissionDefinition(5, "Of Production", 1, nameof(Level.Neptune2OfProduction), nameof(Level.Neptune2Ending), nameof(Level.Neptune2Ending), level => level.Neptune2OfProduction()),
+                new MissionDefinition(6, "Pressing Forward", 1, nameof(Level.Neptune3PressingForwardCampaign), nameof(Level.Neptune3Ending), nameof(Level.Neptune3Ending), level => level.Neptune3PressingForwardCampaign()),
+                new MissionDefinition(7, "Minesweeper", 2, nameof(Level.Titania1MinesweeperCampaign), nameof(Level.Titania1MinesweeperEnding), nameof(Level.Titania1MinesweeperEnding), level => level.Titania1MinesweeperCampaign(), true, AutomatedScenarioStatus.InDevelopment),
+                new MissionDefinition(8, "Beenoculars", 2, nameof(Level.Titania2BeenocularsCampaign), nameof(Level.Titania2CampaignEnding), nameof(Level.Titania2CampaignEnding), level => level.Titania2BeenocularsCampaign(), true, AutomatedScenarioStatus.InDevelopment),
                 // Server-backed campaign_levels_data contains these Uranus missions. Their full
                 // isolated Configure() scenarios remain in development because they still depend on
                 // persistent fleet/UI state that the scenario host does not yet provide.
-                new MissionDefinition(9, "On the Offensive", nameof(Level.Uranus1OnTheOffensive), nameof(Level.Uranus1Ending), nameof(Level.Uranus1Ending), level => level.Uranus1OnTheOffensive(), true, AutomatedScenarioStatus.InDevelopment),
-                new MissionDefinition(10, "On the Defensive", nameof(Level.Uranus2OnTheDefensive), nameof(Level.Uranus2Ending), nameof(Level.Uranus2Ending), level => level.Uranus2OnTheDefensive(), true, AutomatedScenarioStatus.InDevelopment),
-                new MissionDefinition(11, "A New Threat", nameof(Level.Uranus3ANewThreat), nameof(Level.Uranus3Ending), nameof(Level.Uranus3Ending), level => level.Uranus3ANewThreat(), true, AutomatedScenarioStatus.InDevelopment),
+                new MissionDefinition(9, "On the Offensive", 3, nameof(Level.Uranus1OnTheOffensive), nameof(Level.Uranus1Ending), nameof(Level.Uranus1Ending), level => level.Uranus1OnTheOffensive(), true, AutomatedScenarioStatus.InDevelopment),
+                new MissionDefinition(10, "On the Defensive", 3, nameof(Level.Uranus2OnTheDefensive), nameof(Level.Uranus2Ending), nameof(Level.Uranus2Ending), level => level.Uranus2OnTheDefensive(), true, AutomatedScenarioStatus.InDevelopment),
+                new MissionDefinition(11, "A New Threat", 3, nameof(Level.Uranus3ANewThreat), nameof(Level.Uranus3Ending), nameof(Level.Uranus3Ending), level => level.Uranus3ANewThreat(), true, AutomatedScenarioStatus.InDevelopment),
             };
 
         public static IEnumerable<MissionDefinition> GetAutomatedScenarioDefinitions()
