cb5669996183b3d7f28c86babe81bb50b8831920
allStackGit <dmoran201@gmail.com>
2026-08-10 06:52:47 -0400

    Cover level-local asteroid destinations

---
+++ Tests/EditMode/CollisionAsteroidCoordinateTests.cs
@@ -0,0 +1,19 @@
+using System.IO;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Bees.Tests.EditMode
+{
+    [TestFixture]
+    [Category("BeesFoundation")]
+    public class CollisionAsteroidCoordinateTests
+    {
+        [Test]
+        public void AsteroidMovementDestinationUsesLevelLocalCoordinates()
+        {
+            string source = File.ReadAllText(Path.Combine(Application.dataPath, "Scripts", "Entities", "CollisionAsteroid.cs"));
+            Assert.That(source, Does.Contain("Utilities.RandomCoordinate(Level, Vector2.zero"));
+            Assert.That(source, Does.Not.Contain("Utilities.RandomCoordinate(Level, Level.GetPosition()"));
+        }
+    }
+}
