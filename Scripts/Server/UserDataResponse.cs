7f435bd9cf71b5023ad58b393d83ea2f7e80e86c
allStackGit <dmoran201@gmail.com>
2026-08-10 22:47:34 -0400

    Parse lossless user IDs in server responses

--- Scripts/Server/UserDataResponse.cs
+++ Scripts/Server/UserDataResponse.cs
@@ -1,19 +1,17 @@
-﻿using System.Collections;
-using UnityEngine;
-
-namespace Assets.Scripts.Server
-{ 
+﻿namespace Assets.Scripts.Server
+{
     public class UserDataResponse : ServerResponse
     {
-        public int UserId;
+        public string UserId;
         public string Filename;
         public string Contents;
 
-        public UserDataResponse(string type, int status, int userId, long hash, float serverLatency, string filename, string contents) : base(type, status, hash, serverLatency)
+        public UserDataResponse(string type, int status, string userId, long hash, float serverLatency, string filename, string contents)
+            : base(type, status, hash, serverLatency)
         {
-            this.UserId = userId;
-            this.Filename = filename;   
-            this.Contents = contents;
+            UserId = userId;
+            Filename = filename;
+            Contents = contents;
         }
     }
 }
