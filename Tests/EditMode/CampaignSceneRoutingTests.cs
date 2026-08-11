8b56e6b656eb2d65ab32637bc045f0ff05e8efe8
allStackGit <dmoran201@gmail.com>
2026-08-11 13:42:15 -0400

    Update campaign routing tests for partial ConfigData

--- Tests/EditMode/CampaignSceneRoutingTests.cs
+++ Tests/EditMode/CampaignSceneRoutingTests.cs
@@ -10,117 +10,75 @@ namespace Bees.Tests.EditMode
     [Category("BeesFoundation")]
     public class CampaignSceneRoutingTests
     {
-        private Type _configDataType;
-        private Type _routerType;
-        private MethodInfo _shouldRedirect;
-        private object _originalGameMode;
-        private object _originalLevelOptions;
-        private object _originalTestingLevel;
-        private object _originalSeenIntro;
+        private Type _catalogType;
+        private MethodInfo _shouldSkipIntro;
 
         [SetUp]
         public void SetUp()
         {
-            _configDataType = RuntimeAssembly.GetType("Assets.Scripts.ConfigData");
-            _routerType = RuntimeAssembly.GetType("Assets.Scripts.Scenes.CampaignSceneRouter");
-            _shouldRedirect = _routerType.GetMethod(
-                "ShouldRedirectToLevelIntro",
-                BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
-
-            _originalGameMode = RuntimeAssembly.GetStaticField(_configDataType, "CurrentGameMode");
-            _originalLevelOptions = RuntimeAssembly.GetStaticField(_configDataType, "LevelOptions");
-            _originalTestingLevel = RuntimeAssembly.GetStaticField(_configDataType, "IsTestingLevel");
-            _originalSeenIntro = RuntimeAssembly.GetStaticField(_configDataType, "HasSeenPreLevelIntro");
+            _catalogType = RuntimeAssembly.GetType("Assets.Scripts.Levels.CampaignMissionCatalog");
+            _shouldSkipIntro = _catalogType.GetMethod(
+                "ShouldSkipPreLevelIntroForTesting",
+                BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
         }
 
-        [TearDown]
-        public void TearDown()
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        [TestCase(6)]
+        public void ReadyCampaignMissionsKeepAuthoredIntroFlow(int missionId)
         {
-            RuntimeAssembly.SetStaticField(_configDataType, "CurrentGameMode", _originalGameMode);
-            RuntimeAssembly.SetStaticField(_configDataType, "LevelOptions", _originalLevelOptions);
-            RuntimeAssembly.SetStaticField(_configDataType, "IsTestingLevel", _originalTestingLevel);
-            RuntimeAssembly.SetStaticField(_configDataType, "HasSeenPreLevelIntro", _originalSeenIntro);
-        }
-
-        [Test]
-        public void PendingCampaignBattleRedirectsSquadMakerToAuthoredIntro()
-        {
-            ConfigureCampaignMission(2, false, false);
-            Assert.That(ShouldRedirect("Squad Maker"), Is.True);
-        }
-
-        [Test]
-        public void CompletedIntroAllowsCampaignSquadSelection()
-        {
-            ConfigureCampaignMission(2, false, true);
-            Assert.That(ShouldRedirect("Squad Maker"), Is.False);
-        }
-
-        [Test]
-        public void AcceptedCampaignSquadMakerConsumesIntroPermission()
-        {
-            string source = ReadRouterSource();
-            int seenIntroBranch = source.IndexOf("if (ConfigData.HasSeenPreLevelIntro)", StringComparison.Ordinal);
-            int consume = source.IndexOf("ConfigData.HasSeenPreLevelIntro = false;", seenIntroBranch, StringComparison.Ordinal);
-            int returnStatement = source.IndexOf("return;", seenIntroBranch, StringComparison.Ordinal);
-
-            Assert.That(seenIntroBranch, Is.GreaterThanOrEqualTo(0));
-            Assert.That(consume, Is.GreaterThan(seenIntroBranch));
-            Assert.That(returnStatement, Is.GreaterThan(consume));
-        }
-
-        [Test]
-        public void PendingCampaignIntroReplacesSquadMakerBeforeItCanRender()
-        {
-            string source = ReadRouterSource();
-            Assert.That(source, Does.Contain("SceneManager.LoadScene(LevelIntroScene, LoadSceneMode.Single);"));
-            Assert.That(source, Does.Not.Contain("SceneManager.LoadSceneAsync(LevelIntroScene, LoadSceneMode.Single);"),
-                "The intro redirect must complete synchronously so Squad Maker cannot render before the intro.");
-        }
-
-        [Test]
-        public void TestLevelsDoNotRedirectIntoCampaignIntro()
-        {
-            ConfigureCampaignMission(2, true, false);
-            Assert.That(ShouldRedirect("Squad Maker"), Is.False);
+            Assert.That(ShouldSkipIntro(missionId), Is.False);
         }
 
         [TestCase(7)]
         [TestCase(8)]
-        public void TitaniaMissionsMarkedForTestingBypassPreLevelIntro(int missionId)
+        [TestCase(9)]
+        [TestCase(10)]
+        [TestCase(11)]
+        public void InDevelopmentCampaignMissionsBypassPreLevelIntro(int missionId)
         {
-            ConfigureCampaignMission(missionId, false, false);
-            Assert.That(ShouldRedirect("Squad Maker"), Is.False);
+            Assert.That(ShouldSkipIntro(missionId), Is.True);
         }
 
         [Test]
-        public void NonTestingMissionAfterTitaniaStillReceivesPreLevelIntro()
+        public void CampaignLoadingUsesTestingStatusInsteadOfUnconditionalReturn()
         {
-            ConfigureCampaignMission(9, false, false);
-            Assert.That(ShouldRedirect("Squad Maker"), Is.True);
+            string source = ReadCampaignSource();
+
+            Assert.That(source, Does.Contain("CampaignMissionCatalog.ShouldSkipPreLevelIntroForTesting(currentLevel)"));
+            Assert.That(source, Does.Contain("bool skipIntroForTesting = IsTestingLevel ||"));
+            Assert.That(source, Does.Contain("else if (!HasSeenPreLevelIntro)"));
+            Assert.That(source, Does.Contain("SceneManager.LoadSceneAsync(\"Level Intro\", LoadSceneMode.Single);"));
+            Assert.That(source, Does.Contain("SceneManager.LoadSceneAsync(\"Squad Maker\", LoadSceneMode.Single);"));
+            Assert.That(source, Does.Not.Contain("SceneManager.LoadSceneAsync(\"Squad Maker\", LoadSceneMode.Single);\n                    return;"),
+                "Campaign routing must not unconditionally bypass the intro for every later mission.");
         }
 
-        private void ConfigureCampaignMission(int missionId, bool isTestingLevel, bool hasSeenIntro)
+        [Test]
+        public void ConfigDataCampaignRoutingLivesInDedicatedPartial()
         {
-            object campaignMode = Enum.Parse(RuntimeAssembly.GetType("Assets.Scripts.ConfigData+GameModes"), "Campaign");
-            object levelOptions = RuntimeAssembly.CreateUninitialized("Assets.Scripts.Data.LevelOptions");
-            RuntimeAssembly.SetField(levelOptions, "Id", missionId);
+            string core = File.ReadAllText(Path.Combine(Application.dataPath, "Scripts", "ConfigData.cs"));
+            string campaign = ReadCampaignSource();
 
-            RuntimeAssembly.SetStaticField(_configDataType, "CurrentGameMode", campaignMode);
-            RuntimeAssembly.SetStaticField(_configDataType, "LevelOptions", levelOptions);
-            RuntimeAssembly.SetStaticField(_configDataType, "IsTestingLevel", isTestingLevel);
-            RuntimeAssembly.SetStaticField(_configDataType, "HasSeenPreLevelIntro", hasSeenIntro);
+            Assert.That(core, Does.Contain("public static partial class ConfigData"));
+            Assert.That(campaign, Does.Contain("public static partial class ConfigData"));
+            Assert.That(core, Does.Not.Contain("public static void LoadLevel()"));
+            Assert.That(campaign, Does.Contain("public static void LoadLevel()"));
         }
 
-        private static string ReadRouterSource()
+        private bool ShouldSkipIntro(int missionId)
+        {
+            return (bool)_shouldSkipIntro.Invoke(null, new object[] { missionId });
+        }
+
+        private static string ReadCampaignSource()
         {
             return File.ReadAllText(Path.Combine(
-                Application.dataPath, "Scripts", "Scenes", "CampaignSceneRouter.cs"));
-        }
-
-        private bool ShouldRedirect(string sceneName)
-        {
-            return (bool)_shouldRedirect.Invoke(null, new object[] { sceneName });
+                Application.dataPath, "Scripts", "ConfigData.Campaign.cs"));
         }
     }
 }
