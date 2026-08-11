daac85beaf6dd9f0b140a4ea3052a9a72038179f
allStackGit <dmoran201@gmail.com>
2026-08-10 21:29:07 -0400

    Test Hive Mind sighting lifecycle

---
+++ Tests/EditMode/HiveMindVisionLifecycleTests.cs
@@ -0,0 +1,37 @@
+using System.IO;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Bees.Tests.EditMode
+{
+    [TestFixture]
+    [Category("BeesFoundation")]
+    public class HiveMindVisionLifecycleTests
+    {
+        [Test]
+        public void SightingIsRecordedBeforeCommandRewardEligibilityIsChecked()
+        {
+            string path = Path.Combine(Application.dataPath, "Scripts", "Entities", "Ships", "Weapons", "HivemindVision.cs");
+            string source = File.ReadAllText(path);
+
+            int methodStart = source.IndexOf("private void RecordSighting(");
+            Assert.That(methodStart, Is.GreaterThanOrEqualTo(0));
+
+            int addVisibility = source.IndexOf("observerVisibility.Add(_shipEnter);", methodStart);
+            int commandEligibility = source.IndexOf("!rewardSquad.HasCommand", methodStart);
+            Assert.That(addVisibility, Is.GreaterThan(methodStart));
+            Assert.That(commandEligibility, Is.GreaterThan(addVisibility));
+        }
+
+        [Test]
+        public void SightingRejectsDeadFriendlyAndNonShipContacts()
+        {
+            string path = Path.Combine(Application.dataPath, "Scripts", "Entities", "Ships", "Weapons", "HivemindVision.cs");
+            string source = File.ReadAllText(path);
+
+            StringAssert.Contains("_shipEnter == null", source);
+            StringAssert.Contains("_shipEnter.IsDead", source);
+            StringAssert.Contains("_shipEnter.Side == Ship.Side", source);
+        }
+    }
+}
