f12ab4b2add14822b4decc79e09c18c7ca49379e
allStackGit <dmoran201@gmail.com>
2026-08-10 22:47:23 -0400

    Preserve 64-bit user IDs in storage requests

--- Scripts/Server/StoreUserData.cs
+++ Scripts/Server/StoreUserData.cs
@@ -1,21 +1,20 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using System.Globalization;
 
 namespace Assets.Scripts.Server
 {
     public class StoreUserData
     {
-        public readonly ulong UserId;
-        public readonly string DataFile; // the name of the JSON chunk that will be fetched, equivilent to the file name if it was stored locally
+        public readonly string UserId;
+        public readonly string DataFile; // the name of the JSON chunk that will be fetched, equivalent to the file name if it was stored locally
         public readonly string Contents;
         public string Type;
         public long Hash;
+
         public StoreUserData(ulong userId, string dataFile, string contents)
         {
-            this.UserId = userId;
-            this.DataFile = dataFile;
-            this.Contents = contents;
+            UserId = userId.ToString(CultureInfo.InvariantCulture);
+            DataFile = dataFile;
+            Contents = contents;
         }
-
     }
 }
