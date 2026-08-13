10c12d7b7260343bb273734a78bcdc1ac9db711e
allStackGit <dmoran201@gmail.com>
2026-08-13 18:09:17 -0400

    Reuse stable timer snapshots across frames

--- Scripts/Levels/Level.Runtime.cs
+++ Scripts/Levels/Level.Runtime.cs
@@ -21,7 +21,6 @@ namespace Assets.Scripts.Levels
         /// </summary>
         internal int EvaluateCampaignTriggers()
         {
-            //Debug.Log($"Checking triggers");
             Triggers.AddRange(NextTriggers);
             NextTriggers.Clear();
 
@@ -41,59 +40,39 @@ namespace Assets.Scripts.Levels
                     Triggers.RemoveAt(i);
                 }
             }
-            if (!HasContinuousTriggers && Triggers.Count == 0) {
+            if (!HasContinuousTriggers && Triggers.Count == 0)
+            {
                 CancelTimer(_checkTriggersTimer);
-                //CancelInvoke(nameof(CheckTriggers));
             }
             return triggeredCount;
         }
+
         private int _updateIndex;
-        //public HashSet<long> _currentTimerIDs = new HashSet<long>(); // [debug]
-        //private int _removeIndex;
+        private int _timerCollectionVersion;
+        private int _loopTimerVersion = -1;
+
         public void CancelTimer(ScaledTimer scaledTimer)
         {
-            //_removeIndex = Timers.IndexOf(scaledTimer);
-            //if (_removeIndex < 0)
-            //{
-            //    Debug.LogWarning($"Could not find {scaledTimer} in Timers and couldn't remove it");
-            //}
-            //else
-            //{
-            //    Timers.RemoveAt(_removeIndex);
-            //}
-            Timers.Remove(scaledTimer);
-            //_currentTimerIDs.Remove(scaledTimer.Id);
+            if (Timers.Remove(scaledTimer))
+            {
+                _timerCollectionVersion++;
+            }
             scaledTimer.IsCanceled = true;
-            //Debug.Log($"Canceled {scaledTimer}");
         }
+
         public void AddTimer(ScaledTimer scaledTimer)
         {
-            //Debug.Log($"Adding {scaledTimer}");
-            //if (_currentTimerIDs.Contains(scaledTimer.Id)) // [debug]
-            //{
-            //    Debug.LogWarning($"Tried to add {scaledTimer} but it already exists in Timers. Adding anyways");
-            //}
-            //else
-            //{
-            //    Debug.Log($"Adding fresh {scaledTimer} to timers");
-            //}
             Timers.Add(scaledTimer);
-            //_currentTimerIDs.Add(scaledTimer.Id); // [debug]
+            _timerCollectionVersion++;
         }
+
         private readonly List<ScaledTimer> _loopTimers = new List<ScaledTimer>();
         void Update()
         {
-            //GameObject.Find("Rotated Point").transform.position = Utilities.RotatePointAroundPoint(GameObject.Find("Pivot").transform.position, __OriginalPosition, __RotationTest);
-            //if (UseRLServer)
-            //{
-            //    RLSocket.Update();
-            //}
-
-            if (State.GameOver && !State.LevelEnded /*&& !State.CanShipsKeepMining()*/) // Turn this back on when the hivemind is better trained at mining
+            if (State.GameOver && !State.LevelEnded)
             {
                 LevelOver();
                 return;
-
             }
             if ((State.IsPaused || ConfigData.SocketManager.NetworkDisconnection.IsOpen || !IsLevelConnectedToServer) && !Stage.IsTraining)
             {
@@ -111,51 +90,48 @@ namespace Assets.Scripts.Levels
                     _hasSetTimeoutTimer = true;
                     if (Stage.TimeoutTime > 0)
                     {
-                        //CancelTimer(_timeoutTimer);
                         _timeoutTimer.Reuse(Stage.TimeoutTime, LevelTimeOut);
                         AddTimer(_timeoutTimer);
-                        //Debug.Log($"Added timeout timer:{_timeoutTimer}");
-                        //Debug.Log(Utilities.ListToString(Timers));
                     }
                 }
                 if (HasObstacles)
                 {
-                    //Debug.Log($"Calling path finder update again");
                     Pathfinder.Update();
                 }
                 UpdateTimers();
-
             }
-
-
-
         }
+
         public void UpdateTimers()
         {
-            if (Timers.Count > 0)
+            if (Timers.Count == 0)
+            {
+                return;
+            }
+
+            if (_loopTimerVersion != _timerCollectionVersion)
             {
                 _loopTimers.Clear();
                 _loopTimers.AddRange(Timers);
+                _loopTimerVersion = _timerCollectionVersion;
+            }
 
-                for (_updateIndex = 0; _updateIndex < _loopTimers.Count; _updateIndex++)
+            for (_updateIndex = 0; _updateIndex < _loopTimers.Count; _updateIndex++)
+            {
+                ScaledTimer timer = _loopTimers[_updateIndex];
+                if (timer.Update() && !timer.IsRecurring && !timer.IsCanceled)
                 {
-                    if (_loopTimers[_updateIndex].Update() && !_loopTimers[_updateIndex].IsRecurring && !_loopTimers[_updateIndex].IsCanceled)
-                    {
-                        CancelTimer(_loopTimers[_updateIndex]);
-                    }
+                    CancelTimer(timer);
                 }
-
-
             }
         }
+
         private double _timeDouble, _levelOver_fps, _levelOver_fups;
         private ScaledTimer _saveAndEndHalfSecond = new ScaledTimer();
         private ScaledTimer _saveAndEndFiveSecond = new ScaledTimer();
 
         private void RecordPlayerLevelResult()
         {
-            // Automated training episodes are simulation state. They still need WinningSide for
-            // command/outcome accounting, but they must never mutate or persist player progression.
             if (Stage.IsTraining)
             {
                 DidUserWin = false;
@@ -213,14 +189,13 @@ namespace Assets.Scripts.Levels
         /// <summary>
         /// Ends the level and marks the winner
         /// </summary>
-        public void LevelOver() // [stats-method] [note]
+        public void LevelOver()
         {
             if (!Stage.IsTrainingNueralNetwork)
             {
                 Stage.DebugLogger.__LevelCompletes++;
                 State.LevelEnded = true;
                 Pause();
-                //Debug.Log("LEVEL OVER!");
 
                 State.GetAllSquads().ForEach((squad) =>
                 {
@@ -234,7 +209,6 @@ namespace Assets.Scripts.Levels
                 _levelOver_fps = Time.frameCount / (_timeDouble > 0 ? _timeDouble : 0.0000000000000000001);
                 _levelOver_fups = Stage.FixedUpdates / (_timeDouble > 0 ? _timeDouble : 0.0000000000000000001);
                 ConfigData.__TotalLength += Time.realtimeSinceStartup - Stage.StartTime;
-                //ConfigData.__AverageTimeOnQueue = ConfigData.__TotalTimeOnQueue / ConfigData.__TotalRequests;
                 ConfigData.__AverageC2C = ConfigData.__TotalC2C / ConfigData.__TotalRequests;
                 ConfigData.__AverageWireTime = ConfigData.__TotalWireTime / ConfigData.__TotalRequests;
                 ConfigData.__AverageProcessingTime = ConfigData.__TotalProcessingTime / ConfigData.__TotalRequests;
@@ -251,12 +225,10 @@ namespace Assets.Scripts.Levels
 
                 if (State.IsSideKilled(ConfigData.Configuration.BeeSide) && !State.IsSideKilled(ConfigData.Configuration.HumanSide))
                 {
-                    //Debug.Log($"Humans won!");
                     WinningSide = ConfigData.Configuration.HumanSide;
                 }
                 else if (State.IsSideKilled(ConfigData.Configuration.HumanSide) && !State.IsSideKilled(ConfigData.Configuration.BeeSide))
                 {
-                    //Debug.Log($"Bees won!");
                     WinningSide = ConfigData.Configuration.BeeSide;
                 }
                 else if (ConfigData.CurrentGameMode != ConfigData.GameModes.Campaign)
@@ -282,32 +254,26 @@ namespace Assets.Scripts.Levels
 
             if (Stage.IsTrainingNueralNetwork)
             {
-                
                 ResetLevel(false);
             }
             else
             {
                 if (Stage.IsTrainingHiveMind)
                 {
-                    SaveAndEnd(); // invoke immediately because training is happening
-
+                    SaveAndEnd();
                 }
                 else
                 {
                     if (State.FireBargeExplosions.Count > 0)
                     {
-                        //Invoke(nameof(SaveAndEnd), 5f); // invoke after 5 seconds because the explosion should be fully seen
                         _saveAndEndFiveSecond.Reuse(5f, SaveAndEnd);
                         AddTimer(_saveAndEndFiveSecond);
-
                     }
                     else
                     {
                         _saveAndEndHalfSecond.Reuse(.5f, SaveAndEnd);
                         AddTimer(_saveAndEndHalfSecond);
-                        //Invoke(nameof(SaveAndEnd), .5f); // inoke after half a second 
                     }
-
                 }
             }
         }
