ec9e4a88f7baaafd86a0cd5f02578bf25d397a8e
allStackGit <dmoran201@gmail.com>
2026-08-13 18:30:21 -0400

    Avoid LINQ in circling timer

--- Scripts/Levels/Commands/CircleSquad.cs
+++ Scripts/Levels/Commands/CircleSquad.cs
@@ -1,5 +1,6 @@
 ﻿
-using System.Linq;
+using Assets.Scripts.Entities.Ships;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Assets.Scripts.Levels.Commands
@@ -41,6 +42,19 @@ namespace Assets.Scripts.Levels.Commands
             _lastDestination = Vector2.zero;
         }
 
+        private static bool IsAnyShipPathfinding(Squad squad)
+        {
+            List<Ship> ships = squad.GetShips();
+            for (int i = 0; i < ships.Count; i++)
+            {
+                if (ships[i].IsPathfinding)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
         private Vector2 _timer_squadPosition;
         private Vector2 _timer_destination;
         private int _timer_loops;
@@ -48,62 +62,61 @@ namespace Assets.Scripts.Levels.Commands
 
         private void Timer()
         {
-            if (!GetSquad().IsDead)
+            Squad squad = GetSquad();
+            if (squad.IsDead)
             {
-                if (!EnemySquad.IsDead)
+                return;
+            }
+            if (EnemySquad.IsDead)
+            {
+                SetFinalize("The enemy squad is gone or dead");
+                return;
+            }
+
+            squad.Status = $"Moving to circle enemy squad #{EnemySquad.SquadNumber}";
+            if (!_gotToEnemy && !squad.AreSomeSquadShipsWithinRangeOfAllOfOurSquadShips(EnemySquad))
+            {
+                squad.Status = $"Trying to get to a good circling position against {EnemySquad.Name}";
+                if (!IsAnyShipPathfinding(squad))
                 {
-                    GetSquad().Status = $"Moving to circle enemy squad #{EnemySquad.SquadNumber}";
-                    if (!_gotToEnemy && !GetSquad().AreSomeSquadShipsWithinRangeOfAllOfOurSquadShips(EnemySquad))
-                    {
-                        GetSquad().Status = $"Trying to get to a good circling position against {EnemySquad.Name}";
-                        if (!GetSquad().GetShips().Any(ship => ship.IsPathfinding))
-                        {
-                            SetAndMove(EnemySquad.GetPosition());
-                        }
-                    }
-                    else
-                    {
-                        _timer_squadPosition = GetSquad().GetPosition();
-                        if (!_hasSetIdealDistance)
-                        {
-                            _idealDistance = Mathf.Max(EnemySquad.DistanceToPoint(_timer_squadPosition), GetSquad().MaxRange * .5f);
-                            _angle = EnemySquad.AngleToPoint(_timer_squadPosition) - (Mathf.PI * .5f);
-                            _hasSetIdealDistance = true;
-                        }
-                        if (!_gotToEnemy)
-                        {
-                            Level.CancelTimer(CommandTimer);
-                            CommandFrequency = .25f;
-                            _gotToEnemy = true;
-                            CommandTimer.Reuse(CommandFrequency, Timer, true);
-                            Level.AddTimer(CommandTimer);
-                        }
-
-                        _timer_angle = EnemySquad.AngleToPoint(_timer_squadPosition);
-                        _angle = _timer_angle + (.06f * Mathf.PI);
-                        GetSquad().Status = $"Circling enemy squad {EnemySquad.Name} at {_idealDistance} away";
-
-                        _timer_destination = EnemySquad.CirclePoint(_angle, _idealDistance);
-                        _timer_loops = 0;
-
-                        while (Vector2.Distance(_timer_destination, _lastDestination) < 1.25f && _timer_loops < 100)
-                        {
-                            _timer_loops++;
-                            _angle += (.06f * Mathf.PI);
-                            _timer_destination = EnemySquad.CirclePoint(_angle, _idealDistance);
-                        }
-
-                        _lastDestination = _timer_destination;
-                        if (!GetSquad().GetShips().Any(ship => ship.IsPathfinding))
-                        {
-                            SetAndMove(_timer_destination);
-                        }
-                    }
+                    SetAndMove(EnemySquad.GetPosition());
                 }
-                else
-                {
-                    SetFinalize("The enemy squad is gone or dead");
-                }
+                return;
+            }
+
+            _timer_squadPosition = squad.GetPosition();
+            if (!_hasSetIdealDistance)
+            {
+                _idealDistance = Mathf.Max(EnemySquad.DistanceToPoint(_timer_squadPosition), squad.MaxRange * .5f);
+                _angle = EnemySquad.AngleToPoint(_timer_squadPosition) - (Mathf.PI * .5f);
+                _hasSetIdealDistance = true;
+            }
+            if (!_gotToEnemy)
+            {
+                Level.CancelTimer(CommandTimer);
+                CommandFrequency = .25f;
+                _gotToEnemy = true;
+                CommandTimer.Reuse(CommandFrequency, Timer, true);
+                Level.AddTimer(CommandTimer);
+            }
+
+            _timer_angle = EnemySquad.AngleToPoint(_timer_squadPosition);
+            _angle = _timer_angle + (.06f * Mathf.PI);
+            squad.Status = $"Circling enemy squad {EnemySquad.Name} at {_idealDistance} away";
+
+            _timer_destination = EnemySquad.CirclePoint(_angle, _idealDistance);
+            _timer_loops = 0;
+            while (Vector2.Distance(_timer_destination, _lastDestination) < 1.25f && _timer_loops < 100)
+            {
+                _timer_loops++;
+                _angle += (.06f * Mathf.PI);
+                _timer_destination = EnemySquad.CirclePoint(_angle, _idealDistance);
+            }
+
+            _lastDestination = _timer_destination;
+            if (!IsAnyShipPathfinding(squad))
+            {
+                SetAndMove(_timer_destination);
             }
         }
     }
