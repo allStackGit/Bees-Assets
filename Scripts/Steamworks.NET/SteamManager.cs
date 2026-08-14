0a3b88745f938d5e24aa8d05730e92ea6528f1c4
allStackGit <dmoran201@gmail.com>
2026-08-14 17:05:33 -0400

    fix: make Steam optional and validate Windows packages

--- Scripts/Steamworks.NET/SteamManager.cs
+++ Scripts/Steamworks.NET/SteamManager.cs
@@ -37,9 +37,20 @@ public class SteamManager : MonoBehaviour {
 	}
 
 	protected bool m_bInitialized = false;
+	protected bool m_bInitializationFailed = false;
 	public static bool Initialized {
 		get {
 			return Instance.m_bInitialized;
+		}
+	}
+
+	/// <summary>
+	/// True after this process attempted to initialize Steam and could not. Steam is an optional
+	/// platform service for Bees startup; callers should fall back instead of blocking the game.
+	/// </summary>
+	public static bool InitializationFailed {
+		get {
+			return Instance.m_bInitializationFailed;
 		}
 	}
 
@@ -60,6 +71,21 @@ public class SteamManager : MonoBehaviour {
 	}
 #endif
 
+	private void MarkInitializationFailed(string reason, System.Exception exception = null)
+	{
+		m_bInitialized = false;
+		m_bInitializationFailed = true;
+
+		if (exception == null)
+		{
+			Debug.LogWarning($"[Steamworks.NET] {reason} Continuing without Steam features.", this);
+		}
+		else
+		{
+			Debug.LogWarning($"[Steamworks.NET] {reason} Continuing without Steam features. {exception.GetType().Name}: {exception.Message}", this);
+		}
+	}
+
 	protected virtual void Awake() {
 		// Only one instance of SteamManager at a time!
 		if (s_instance != null) {
@@ -69,63 +95,49 @@ public class SteamManager : MonoBehaviour {
 		s_instance = this;
 
 		if(s_EverInitialized) {
-			// This is almost always an error.
-			// The most common case where this happens is when SteamManager gets destroyed because of Application.Quit(),
-			// and then some Steamworks code in some other OnDestroy gets called afterwards, creating a new SteamManager.
-			// You should never call Steamworks functions in OnDestroy, always prefer OnDisable if possible.
-			throw new System.Exception("Tried to Initialize the SteamAPI twice in one session!");
+			// Steam is optional to the rest of the game. A late duplicate manager should not turn a
+			// shutdown/lifecycle ordering issue into an uncaught exception that kills the player.
+			MarkInitializationFailed("Tried to initialize the Steam API twice in one session.");
+			return;
 		}
 
 		// We want our SteamManager Instance to persist across scenes.
 		DontDestroyOnLoad(gameObject);
 
-		if (!Packsize.Test()) {
-			Debug.LogError("[Steamworks.NET] Packsize Test returned false, the wrong version of Steamworks.NET is being run in this platform.", this);
-		}
+		try
+		{
+			if (!Packsize.Test()) {
+				Debug.LogError("[Steamworks.NET] Packsize Test returned false, the wrong version of Steamworks.NET is being run in this platform.", this);
+			}
 
-		if (!DllCheck.Test()) {
-			Debug.LogError("[Steamworks.NET] DllCheck Test returned false, One or more of the Steamworks binaries seems to be the wrong version.", this);
-		}
+			if (!DllCheck.Test()) {
+				Debug.LogError("[Steamworks.NET] DllCheck Test returned false, One or more of the Steamworks binaries seems to be the wrong version.", this);
+			}
 
-		try {
-			// If Steam is not running or the game wasn't started through Steam, SteamAPI_RestartAppIfNecessary starts the
-			// Steam client and also launches this game again if the User owns it. This can act as a rudimentary form of DRM.
-			// Note that this will run which ever version you have installed in steam. Which may not be the precise executable
-			// we were currently running.
-
-			// Once you get a Steam AppID assigned by Valve, you need to replace AppId_t.Invalid with it and
-			// remove steam_appid.txt from the game depot. eg: "(AppId_t)480" or "new AppId_t(480)".
-			// See the Valve documentation for more information: https://partner.steamgames.com/doc/sdk/api#initialization_and_shutdown
+			// If Steam is available and this title needs to be restarted through the Steam client,
+			// preserve Steamworks.NET's normal restart behavior. Failure to load/initialize Steam,
+			// however, is not fatal to Bees and is handled below.
 			if (SteamAPI.RestartAppIfNecessary(AppId_t.Invalid)) {
 				Debug.Log("[Steamworks.NET] Shutting down because RestartAppIfNecessary returned true. Steam will restart the application.");
 
 				Application.Quit();
 				return;
 			}
-		}
-		catch (System.DllNotFoundException e) { // We catch this exception here, as it will be the first occurrence of it.
-			Debug.LogError("[Steamworks.NET] Could not load [lib]steam_api.dll/so/dylib. It's likely not in the correct location. Refer to the README for more details.\n" + e, this);
 
-			Application.Quit();
+			m_bInitialized = SteamAPI.Init();
+		}
+		catch (System.Exception exception)
+		{
+			MarkInitializationFailed("Steam could not be loaded or initialized.", exception);
 			return;
 		}
 
-		// Initializes the Steamworks API.
-		// If this returns false then this indicates one of the following conditions:
-		// [*] The Steam client isn't running. A running Steam client is required to provide implementations of the various Steamworks interfaces.
-		// [*] The Steam client couldn't determine the App ID of game. If you're running your application from the executable or debugger directly then you must have a [code-inline]steam_appid.txt[/code-inline] in your game directory next to the executable, with your app ID in it and nothing else. Steam will look for this file in the current working directory. If you are running your executable from a different directory you may need to relocate the [code-inline]steam_appid.txt[/code-inline] file.
-		// [*] Your application is not running under the same OS user context as the Steam client, such as a different user or administration access level.
-		// [*] Ensure that you own a license for the App ID on the currently active Steam account. Your game must show up in your Steam library.
-		// [*] Your App ID is not completely set up, i.e. in Release State: Unavailable, or it's missing default packages.
-		// Valve's documentation for this is located here:
-		// https://partner.steamgames.com/doc/sdk/api#initialization_and_shutdown
-		m_bInitialized = SteamAPI.Init();
 		if (!m_bInitialized) {
-			Debug.LogError("[Steamworks.NET] SteamAPI_Init() failed. Refer to Valve's documentation or the comment above this line for more information.", this);
-
+			MarkInitializationFailed("SteamAPI_Init() failed.");
 			return;
 		}
 
+		m_bInitializationFailed = false;
 		s_EverInitialized = true;
 	}
 
@@ -176,6 +188,12 @@ public class SteamManager : MonoBehaviour {
 	public static bool Initialized {
 		get {
 			return false;
+		}
+	}
+
+	public static bool InitializationFailed {
+		get {
+			return true;
 		}
 	}
 #endif // !DISABLESTEAMWORKS
