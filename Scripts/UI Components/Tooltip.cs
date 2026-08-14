863340ff82ae0f9176aa2c4f183aaf5b32e97b22
allStackGit <dmoran201@gmail.com>
2026-08-13 20:27:04 -0400

    Close tooltips as soon as the X is pressed

--- Scripts/UI Components/Tooltip.cs
+++ Scripts/UI Components/Tooltip.cs
@@ -1,41 +1,67 @@
 using Assets.Scripts;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Tooltip : MonoBehaviour
 {
-
     public GameObject TooltipObject, CloseButton;
     public TMP_Text TooltipText;
     public RectTransform TooltipPosition;
     public RectTransform TooltipSize;
+    private bool _closePressConfigured;
 
     public void Place(Vector2 position, Vector2 size)
     {
         TooltipPosition.localPosition = position;
         TooltipSize.sizeDelta = size;
     }
+
     public void Show(string text, bool hasX)
     {
         if (ConfigData.UserProgressData.ShowToolTips)
         {
             TooltipText.text = text;
-            // Implement UI logic to display the tooltip with the given text
             Debug.Log($"Showing tooltip: {text}");
 
             CloseButton.SetActive(hasX);
+            ConfigureClosePress();
             TooltipObject.SetActive(true);
         }
         else
         {
             Hide();
         }
-
     }
+
+    private void ConfigureClosePress()
+    {
+        if (_closePressConfigured || CloseButton == null)
+        {
+            return;
+        }
+
+        EventTrigger trigger = CloseButton.GetComponent<EventTrigger>();
+        if (trigger == null)
+        {
+            trigger = CloseButton.AddComponent<EventTrigger>();
+        }
+        if (trigger.triggers == null)
+        {
+            trigger.triggers = new System.Collections.Generic.List<EventTrigger.Entry>();
+        }
+
+        EventTrigger.Entry press = new EventTrigger.Entry
+        {
+            eventID = EventTriggerType.PointerDown
+        };
+        press.callback.AddListener(_ => Hide());
+        trigger.triggers.Add(press);
+        _closePressConfigured = true;
+    }
+
     public void Hide()
     {
-        // Implement UI logic to hide the tooltip
         TooltipObject.SetActive(false);
     }
-
 }
