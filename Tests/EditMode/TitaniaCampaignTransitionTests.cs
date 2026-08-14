e75c19eddf1a7c4e5ec705019ba18d7e1430b8e3
allStackGit <dmoran201@gmail.com>
2026-08-14 13:06:25 -0400

    Cover Titania II fail-forward progression

--- Tests/EditMode/TitaniaCampaignTransitionTests.cs
+++ Tests/EditMode/TitaniaCampaignTransitionTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using NUnit.Framework;
 using UnityEngine;
@@ -11,24 +12,79 @@ namespace Bees.Tests.EditMode
         [Test]
         public void BeenocularsEndingRestoresPlanetIntermissionStateBeforeAdvancing()
         {
-            string source = File.ReadAllText(Path.Combine(
-                Application.dataPath,
-                "Scripts",
-                "Levels",
-                "Titania2Beenoculars.cs"));
+            string source = ReadBeenocularsSource();
+            string endingSource = ExtractMethodBody(source, "public void Titania2CampaignEnding()");
 
-            int ending = source.IndexOf("public void Titania2CampaignEnding()");
-            Assert.That(ending, Is.GreaterThanOrEqualTo(0));
-            string endingSource = source.Substring(ending);
-
-            int resetIntro = endingSource.IndexOf("ConfigData.HasSeenPreLevelIntro = false;");
-            int resetIntermission = endingSource.IndexOf("ConfigData.HasSeenIntermission = false;");
-            int advance = endingSource.IndexOf("ConfigData.UserProgressData.AdvanceToNextLevel();");
+            int resetIntro = endingSource.IndexOf("ConfigData.HasSeenPreLevelIntro = false;", StringComparison.Ordinal);
+            int resetIntermission = endingSource.IndexOf("ConfigData.HasSeenIntermission = false;", StringComparison.Ordinal);
+            int advance = endingSource.IndexOf("ConfigData.UserProgressData.AdvanceToNextLevel();", StringComparison.Ordinal);
 
             Assert.That(resetIntro, Is.GreaterThanOrEqualTo(0));
             Assert.That(resetIntermission, Is.GreaterThanOrEqualTo(0));
             Assert.That(advance, Is.GreaterThan(resetIntro));
             Assert.That(advance, Is.GreaterThan(resetIntermission));
+        }
+
+        [Test]
+        public void BeenocularsDefeatAlsoAdvancesToUranus()
+        {
+            string source = ReadBeenocularsSource();
+            string endingSource = ExtractMethodBody(source, "public void Titania2CampaignEnding()");
+
+            int victoryCondition = endingSource.IndexOf(
+                "if (WinningSide == ConfigData.Configuration.UserSide)",
+                StringComparison.Ordinal);
+            Assert.That(victoryCondition, Is.GreaterThanOrEqualTo(0));
+
+            int victoryBlockStart = endingSource.IndexOf('{', victoryCondition);
+            int victoryBlockEnd = FindMatchingBrace(endingSource, victoryBlockStart);
+            int advance = endingSource.IndexOf(
+                "ConfigData.UserProgressData.AdvanceToNextLevel();",
+                StringComparison.Ordinal);
+            int checkpoint = endingSource.IndexOf("CampaignCheckpoint.Save();", StringComparison.Ordinal);
+
+            Assert.That(advance, Is.GreaterThan(victoryBlockEnd),
+                "Titania II must fail forward to Uranus even when WinningSide is the AI.");
+            Assert.That(checkpoint, Is.GreaterThan(advance),
+                "The fail-forward campaign level must be checkpointed after advancing.");
+        }
+
+        private static string ReadBeenocularsSource()
+        {
+            return File.ReadAllText(Path.Combine(
+                Application.dataPath,
+                "Scripts",
+                "Levels",
+                "Titania2Beenoculars.cs"));
+        }
+
+        private static string ExtractMethodBody(string source, string signature)
+        {
+            int method = source.IndexOf(signature, StringComparison.Ordinal);
+            Assert.That(method, Is.GreaterThanOrEqualTo(0));
+            int openingBrace = source.IndexOf('{', method);
+            int closingBrace = FindMatchingBrace(source, openingBrace);
+            return source.Substring(openingBrace, closingBrace - openingBrace + 1);
+        }
+
+        private static int FindMatchingBrace(string source, int openingBrace)
+        {
+            Assert.That(openingBrace, Is.GreaterThanOrEqualTo(0));
+            int depth = 0;
+            for (int i = openingBrace; i < source.Length; i++)
+            {
+                if (source[i] == '{')
+                {
+                    depth++;
+                }
+                else if (source[i] == '}' && --depth == 0)
+                {
+                    return i;
+                }
+            }
+
+            Assert.Fail("Could not find matching closing brace.");
+            return -1;
         }
     }
 }
