7cad8b57c44d9398fea4085c969704fb7f255038
allStackGit <dmoran201@gmail.com>
2026-08-16 06:52:59 -0400

    Preview carrier deck variants in squad maker

--- Scripts/UI Components/DragIcon.cs
+++ Scripts/UI Components/DragIcon.cs
@@ -1,18 +1,22 @@
 ﻿
 
 using Assets.Scripts.Data;
+using Assets.Scripts.Entities.Ships;
 using Assets.Scripts.Scenes;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 namespace Assets.Scripts.UIComponents
 {
     public class DragIcon
     {
+        private const string CarrierDeckVariantObjectName = "Carrier Deck Variant";
+
         public Vector2 Position => _icon.transform.position;
         public int Id;
         private GameObject _icon;
@@ -20,6 +24,7 @@ namespace Assets.Scripts.UIComponents
         private FleetShip _fleetShip;
         private SquadMaker _scene;
         private int[] _changeablePixels;
+        private Image _carrierDeckVariantImage;
 
         public bool HasDeadShipBox => _deadShipBox != null;
         public DragIcon(SquadMaker scene, GameObject icon, FleetShip fleetShip, string gameObjectName, int id)
@@ -59,19 +64,82 @@ namespace Assets.Scripts.UIComponents
         public void SetChangablePixels(Color[] colors)
         {
             //Debug.Log($"Setting changable pixels for {_icon.name}");
-            UnityEngine.UI.Image image = _icon.GetComponent<UnityEngine.UI.Image>();
+            Image image = _icon.GetComponent<Image>();
             _changeablePixels = Utilities.GetChangablePixelsForImage(colors, image.sprite);
         }
         public void SetColor(Color color)
         {
             if (color.Equals(ConfigData.UnsetColor))
             {
+                SetCarrierDeckVariant(null);
                 return;
             }
-            UnityEngine.UI.Image image = _icon.GetComponent<UnityEngine.UI.Image>();
+            Image image = _icon.GetComponent<Image>();
 
             image.sprite = Utilities.SetImageColor(color, image.sprite, _changeablePixels);
+
+            if (_fleetShip != null && _fleetShip.Type == ConfigData.ShipTypes.Carrier)
+            {
+                SetCarrierDeckVariant(CarrierDeckVariants.GetDeckSprite(color));
+            }
         }
+
+        private void SetCarrierDeckVariant(Sprite deckSprite)
+        {
+            if (_fleetShip == null || _fleetShip.Type != ConfigData.ShipTypes.Carrier)
+            {
+                return;
+            }
+
+            if (deckSprite == null)
+            {
+                if (_carrierDeckVariantImage != null)
+                {
+                    _carrierDeckVariantImage.enabled = false;
+                }
+                return;
+            }
+
+            EnsureCarrierDeckVariantImage();
+            _carrierDeckVariantImage.sprite = deckSprite;
+            _carrierDeckVariantImage.color = Color.white;
+            _carrierDeckVariantImage.enabled = true;
+        }
+
+        private void EnsureCarrierDeckVariantImage()
+        {
+            if (_carrierDeckVariantImage != null)
+            {
+                return;
+            }
+
+            Transform existing = _icon.transform.Find(CarrierDeckVariantObjectName);
+            if (existing != null)
+            {
+                _carrierDeckVariantImage = existing.GetComponent<Image>();
+            }
+
+            if (_carrierDeckVariantImage == null)
+            {
+                GameObject deckObject = new GameObject(
+                    CarrierDeckVariantObjectName,
+                    typeof(RectTransform),
+                    typeof(CanvasRenderer),
+                    typeof(Image));
+                deckObject.transform.SetParent(_icon.transform, false);
+                _carrierDeckVariantImage = deckObject.GetComponent<Image>();
+            }
+
+            RectTransform rectTransform = _carrierDeckVariantImage.rectTransform;
+            rectTransform.anchorMin = Vector2.zero;
+            rectTransform.anchorMax = Vector2.one;
+            rectTransform.offsetMin = Vector2.zero;
+            rectTransform.offsetMax = Vector2.zero;
+            rectTransform.localScale = Vector3.one;
+            _carrierDeckVariantImage.raycastTarget = false;
+            _carrierDeckVariantImage.preserveAspect = false;
+        }
+
         public void SetDeadShipBox(GameObject deadShipBox)
         {
             _deadShipBox = deadShipBox;
