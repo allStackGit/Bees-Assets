58d0399b71d4d818d061dde677e59503a591021d
allStackGit <dmoran201@gmail.com>
2026-08-17 21:28:42 -0400

    Use hash-keyed waitable request tracking

--- Scripts/Server/Socket.cs
+++ Scripts/Server/Socket.cs
@@ -45,7 +45,7 @@ namespace Assets.Scripts.Server
         public HashSet<long> HandledRequests = new HashSet<long>();
         public ConcurrentQueue<byte[]> MessageQueue = new ConcurrentQueue<byte[]>();
         public List<Level> OpenLevels = new List<Level>();
-        private readonly HashSet<ServerRequest> _waitableRequests = new HashSet<ServerRequest>();
+        private readonly ServerRequestSet _waitableRequests = new ServerRequestSet();
         private readonly List<ServerRequest> _waitableRequestSnapshot = new List<ServerRequest>();
 
         private ConcurrentQueue<Action> MainThreadActions
