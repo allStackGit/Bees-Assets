dd53a5c50b7ca1330100d215ab0ed55017bf4be5
allStackGit <dmoran201@gmail.com>
2026-09-09 15:02:05 -0400

    Classify Striker projectile damage in RL logs

--- Scripts/Entities/Projectiles/StrikerBomb.cs
+++ Scripts/Entities/Projectiles/StrikerBomb.cs
@@ -99,7 +99,14 @@ namespace Assets.Scripts.Entities.Projectiles
             // attributing it to the new occupant of that pooled Ship object.
             if (Shooter != null && Shooter.FleetShip != null && Shooter.FleetShip.Id == _shooterFleetShipId)
             {
-                Ship.LogAttackingDamage(Power, Shooter, FleetShip, SavedSquad, ContactedShip, CommandOutcomeId);
+                Ship.LogAttackingDamage(
+                    Power,
+                    Shooter,
+                    FleetShip,
+                    SavedSquad,
+                    ContactedShip,
+                    CommandOutcomeId,
+                    rlDamageSource: "bomb");
             }
             else
             {
