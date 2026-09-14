cf2f229d5fad859f05f41cf92fb2c171e6cec0a3
allStackGit <dmoran201@gmail.com>
2026-09-14 10:39:54 -0400

    Use profile-independent IDs for Scout spawns

--- Scripts/Entities/Ships/Scout.cs
+++ Scripts/Entities/Ships/Scout.cs
@@ -70,7 +70,7 @@ namespace Assets.Scripts.Entities.Ships
                 Squad.IsMatchingSpeed,
                 Squad.ShouldChase(),
                 true,
-                Utilities.GetNegativeSavedSquadId(),
+                TransientIdAllocator.GetSavedSquadId(),
                 Squad.Side,
                 Level.State.OriginalSquadCounts[Side - 1] + 1,
                 $"{Name} - {MinionType} Spawn #{BeaconsDropped}",
@@ -91,7 +91,7 @@ namespace Assets.Scripts.Entities.Ships
 
             global::RlGameplayDemonstrationCapabilityCapture.Record(this, global::RlOneVsOneAgent.ShipSpecialAction);
             TimeSinceLastBeaconDropped = BeaconClock;
-            long id = Utilities.GetNegativeFleetshipId();
+            long id = TransientIdAllocator.GetFleetShipId();
             Beacon ship = (Beacon)Level.LevelConstructor.InstantiateShip(MinionType);
             if (ship == null)
             {
