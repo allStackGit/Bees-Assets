ff19e41ccc2e2fd8814354d59d77057418649a15
allStackGit <dmoran201@protonmail.com>
2026-09-18 17:39:18 -0400

    Test compact permuted RL ship identities

--- Tests/EditMode/RlCommunicationAndMemoryPolicyTests.cs
+++ Tests/EditMode/RlCommunicationAndMemoryPolicyTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 
@@ -20,18 +21,18 @@ namespace Bees.Tests.EditMode
         }
 
         [Test]
-        public void PolicyAbiIncludesExactShipIdentityAndPrivateAlliedCommunication()
+        public void PolicyAbiIncludesEpisodeLocalShipIdentityAndPrivateAlliedCommunication()
         {
             Type agent = RuntimeAssembly.GetType("RlOneVsOneAgent");
-            Assert.That(RuntimeAssembly.GetStaticField(agent, "ShipIdBitCount"), Is.EqualTo(64));
+            Assert.That(RuntimeAssembly.GetStaticField(agent, "ShipIdentityObservationSize"), Is.EqualTo(1));
             Assert.That(RuntimeAssembly.GetStaticField(agent, "CommunicationObservationSize"), Is.EqualTo(4));
             Assert.That(RuntimeAssembly.GetStaticField(agent, "CommunicationContinuousActionCount"), Is.EqualTo(4));
             Assert.That(RuntimeAssembly.GetStaticField(agent, "ContinuousActionCount"), Is.EqualTo(16));
-            Assert.That(RuntimeAssembly.GetStaticField(agent, "ObservationSize"), Is.EqualTo(24356));
+            Assert.That(RuntimeAssembly.GetStaticField(agent, "ObservationSize"), Is.EqualTo(16166));
 
             string perception = Read("Scripts", "Scenes", "RlCombatPerception.cs");
-            Assert.That(perception, Does.Contain("AddShipIdBits(sensor, ship.Id);"));
-            Assert.That(perception, Does.Contain("AddShipIdBits(sensor, observed.Id);"));
+            Assert.That(perception, Does.Contain("AddShipIdentityObservation(sensor, ship);"));
+            Assert.That(perception, Does.Contain("AddShipIdentityObservation(sensor, observed);"));
             Assert.That(perception, Does.Contain("AddAllySlots(sensor, _allyCandidates"));
             Assert.That(perception, Does.Contain("RlOneVsOneAgent.AddCommunicationObservations(sensor, ally);"));
 
@@ -39,6 +40,30 @@ namespace Bees.Tests.EditMode
             int enemySlots = perception.IndexOf("AddEntitySlots(sensor, _enemyCandidates", enemyCollection, StringComparison.Ordinal);
             Assert.That(enemySlots, Is.GreaterThan(enemyCollection),
                 "Enemy observations must use entity slots without the private allied communication tail.");
+        }
+
+        [Test]
+        public void EpisodeShipIdentityUsesOneStableCollisionFreeScalarPerObservedShip()
+        {
+            Type identity = RuntimeAssembly.GetType("RlEpisodeShipIdentity");
+            HashSet<float> values = new HashSet<float>();
+
+            for (int ordinal = 0; ordinal < 130; ordinal++)
+            {
+                float value = (float)RuntimeAssembly.InvokeStatic(identity, "EncodeOrdinal", ordinal, 5, 17);
+                Assert.That(value, Is.InRange(-1f, 1f));
+                Assert.That(values.Add(value), Is.True, $"Duplicate scalar identity at ordinal {ordinal}.");
+            }
+
+            float stable = (float)RuntimeAssembly.InvokeStatic(identity, "EncodeOrdinal", 7, 5, 17);
+            float repeated = (float)RuntimeAssembly.InvokeStatic(identity, "EncodeOrdinal", 7, 5, 17);
+            float nextEpisodePermutation = (float)RuntimeAssembly.InvokeStatic(identity, "EncodeOrdinal", 7, 11, 29);
+            Assert.That(repeated, Is.EqualTo(stable));
+            Assert.That(nextEpisodePermutation, Is.Not.EqualTo(stable));
+
+            string identitySource = Read("Scripts", "Scenes", "RlEpisodeShipIdentity.cs");
+            Assert.That(identitySource, Does.Contain("EpisodeStates.Remove(level);"));
+            Assert.That(identitySource, Does.Contain("RlOneVsOneScenarioSeed.IdentityStreamSalt"));
         }
 
         [Test]
@@ -62,8 +87,8 @@ namespace Bees.Tests.EditMode
             Assert.That(trainer, Does.Contain("sequence_length: 256"));
             Assert.That(trainer, Does.Contain("memory_size: 512"));
             Assert.That(schema, Does.Contain("network=lstm-1024x4-mem512-seq256"));
-            Assert.That(schema, Does.Contain("ship-id=exact-64bit-categorical"));
-            Assert.That(schema, Does.Contain("ally=176-with-private-comm4"));
+            Assert.That(schema, Does.Contain("ship-id=episode-permuted-scalar23"));
+            Assert.That(schema, Does.Contain("ally=113-with-private-comm4"));
         }
     }
 }
