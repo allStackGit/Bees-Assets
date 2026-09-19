d78b9a8c54cc7b0fa9f05af17f97674cf109f6e8
allStackGit <dmoran201@protonmail.com>
2026-09-18 20:19:07 -0400

    Tests: cover unified RL observations

--- Tests/EditMode/RlPolicySchemaContractTests.cs
+++ Tests/EditMode/RlPolicySchemaContractTests.cs
@@ -31,16 +31,20 @@ namespace Bees.Tests.EditMode
             Assert.That(perception, Does.Contain("internal const int MaxObservedAllies = 64;"));
             Assert.That(perception, Does.Contain("internal const int MaxObservedEnemies = 64;"));
             Assert.That(perception, Does.Contain("internal const int MaxWeaponSlots = 16;"));
+            Assert.That(perception, Does.Contain("internal const int MaxObservedEntityWeaponSlots = MaxWeaponSlots;"));
             Assert.That(perception, Does.Contain("internal const int MaxObservedEnemyWeaponMounts = 16;"));
+            Assert.That(perception, Does.Contain("internal const int EntityObservationSize = EntityCoreObservationSize + MaxObservedEntityWeaponSlots * WeaponObservationSize;"));
+            Assert.That(perception, Does.Contain("AddEntityWeaponSlots(observed, sensor, frameQuarterTurns);"));
+            Assert.That(perception, Does.Contain("AddWeaponObservation(ship, ship.Weapons[slot], sensor, frameQuarterTurns);"));
             Assert.That(perception, Does.Contain("internal const int ObjectiveObservationSize = 16;"));
             Assert.That(perception, Does.Contain("internal const int ObservationSize = SelfObservationSize +"));
 
-            Assert.That(schema, Does.Contain("internal const int Version = 7;"));
-            Assert.That(schema, Does.Contain("internal const int PerceptionObservationSize = 4685;"));
+            Assert.That(schema, Does.Contain("internal const int Version = 8;"));
+            Assert.That(schema, Does.Contain("internal const int PerceptionObservationSize = 43549;"));
             Assert.That(schema, Does.Contain("internal const int ReservedObservationCount = 20;"));
             Assert.That(schema, Does.Contain("internal const int ExpectedObservationSize = ReservedObservationEndExclusive;"));
-            Assert.That(schema, Does.Contain("bees-rl-v7"));
-            Assert.That(schema, Does.Contain("obs=4706"));
+            Assert.That(schema, Does.Contain("bees-rl-v8"));
+            Assert.That(schema, Does.Contain("obs=43570"));
             Assert.That(schema, Does.Contain("tail=episode-progress+20-reserved"));
             Assert.That(schema, Does.Contain("coord-frame=team-episode-distinct-quarter-turn"));
             Assert.That(schema, Does.Contain("cont=34"));
