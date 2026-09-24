469e489b50a368c17d28606e3173f832423a80ea
allStackGit <dmoran201@protonmail.com>
2026-09-24 17:39:31 -0400

    Revert development auth bypass wiring

--- BeesServer~/server.js
+++ BeesServer~/server.js
@@ -402,8 +402,6 @@ function createServer(options = {}) {
     installCampaignCheckpoint(runtime);
     const launch = { ...parseLaunchOptions(options.argv || process.argv), ...options };
     const server = new runtime.Server(Boolean(launch.test), launch.port || 7143);
-    server.allowInsecureAuthentication = launch.allowInsecureAuthentication === true ||
-        process.env.BEES_ALLOW_INSECURE_DEVELOPMENT_AUTH === '1';
     server.db = databaseFromLegacyConfig(server.db, launch);
     patchServer(server, runtime);
     applyTestIsolation(server, launch);
