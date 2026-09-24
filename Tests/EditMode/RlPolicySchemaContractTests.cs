88534bc36884a843edf99c41e2140f08d12b091a
allStackGit <dmoran201@protonmail.com>
2026-09-23 23:15:47 -0400

    Update frozen RL ABI contract version

--- Tests/EditMode/RlPolicySchemaContractTests.cs
+++ Tests/EditMode/RlPolicySchemaContractTests.cs
@@ -49,16 +49,16 @@ namespace Bees.Tests.EditMode
             Assert.That(perception, Does.Contain("internal const int ObjectiveObservationSize = 16;"));
             Assert.That(perception, Does.Contain("internal const int ObservationSize = SelfObservationSize +"));
 
-            Assert.That(schema, Does.Contain("internal const int Version = 16;"));
+            Assert.That(schema, Does.Contain("internal const int Version = 17;"));
             Assert.That(schema, Does.Contain("internal const int PerceptionObservationSize = 7065;"));
             Assert.That(schema, Does.Contain("internal const int ReservedObservationCount = 20;"));
             Assert.That(schema, Does.Contain("internal const int ExpectedObservationSize = ReservedObservationEndExclusive;"));
-            Assert.That(schema, Does.Contain("bees-rl-v16"));
+            Assert.That(schema, Does.Contain("bees-rl-v17"));
             Assert.That(schema, Does.Contain("obs=7086"));
             Assert.That(schema, Does.Contain("tail=episode-progress+20-reserved"));
             Assert.That(schema, Does.Contain("coord-frame=team-episode-distinct-quarter-turn"));
             Assert.That(schema, Does.Contain("cont=12"));
-            Assert.That(schema, Does.Contain("disc=2x5,5,65,65,65"));
+            Assert.That(schema, Does.Contain("disc=2x5,5"));
             Assert.That(schema, Does.Contain("weapon-aim=slotwise-xy"));
             Assert.That(schema, Does.Contain("weapon-fire=slotwise-cease-or-fire"));
             Assert.That(schema, Does.Contain("ship-id=episode-permuted-scalar23"));
