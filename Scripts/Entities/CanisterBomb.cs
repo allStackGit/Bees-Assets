0d3424410aab6373e74d7b698cb7b2350d302204
allStackGit <dmoran201@protonmail.com>
2026-09-23 21:07:37 -0400

    Remove non-RL Debug.Log output

--- Scripts/Entities/CanisterBomb.cs
+++ Scripts/Entities/CanisterBomb.cs
@@ -137,7 +137,6 @@ namespace Assets.Scripts.Entities
         {
             if (!IsDead)
             {
-                Debug.Log("Canister bomb killed");
                 Explosion = (RocketExplosion)Level.Stage.Pool.GetProjectileFromPool(ConfigData.ProjectileTypes.FireTankExplosion);
                 Explosion.transform.parent = Level.Map.Transform;
                 Explosion.Setup(Level, LastHitProjectile.Weapon, LastHitProjectile.Shooter, null, transform.localPosition, 0, 0, Power);
