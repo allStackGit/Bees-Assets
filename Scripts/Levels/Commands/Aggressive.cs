c051fb06838262b8d0141d4226a6eea625c8e132
allStackGit <dmoran201@protonmail.com>
2026-09-24 15:37:27 -0400

    Restore aggressive freeze diagnostics

--- Scripts/Levels/Commands/Aggressive.cs
+++ Scripts/Levels/Commands/Aggressive.cs
@@ -101,6 +101,7 @@ namespace Assets.Scripts.Levels.Commands
 
         private void Timer()
         {
+            FreezeDiagnostics.RecordAggressiveTick(Level);
             Squad squad = GetSquad();
             if (squad.IsDead)
             {
