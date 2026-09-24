b6b6625627c4e6cbc75d8a6592bf9c87570039ca
allStackGit <dmoran201@protonmail.com>
2026-09-24 16:37:31 -0400

    Restore endpoint regression contract

--- Tests/EditMode/DevelopmentServerEndpointTests.cs
+++ Tests/EditMode/DevelopmentServerEndpointTests.cs
@@ -15,8 +15,7 @@ namespace Bees.Tests.EditMode
                 Application.dataPath, "Scripts", "ConfigData.cs"));
 
             Assert.That(source, Does.Contain("public const int DevelopmentPort = 7146;"));
-            Assert.That(source, Does.Contain("public const string EditorServerHostname = \"127.0.0.1\";"));
-            Assert.That(source, Does.Contain("#if UNITY_EDITOR"));
+            Assert.That(source, Does.Contain("public const string DevelopmentServerHostname = LocalServerHostname;"));
             Assert.That(source, Does.Contain("_socket = new Socket(DevelopmentPort, DevelopmentServerHostname, UseWebSocketSharp);"));
         }
     }
