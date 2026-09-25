b9d85aeacc54d3248f833fb8a5f7948f5b049d35
allStackGit <dmoran201@protonmail.com>
2026-09-25 07:03:39 -0400

    Guard public development server endpoint

--- Tests/EditMode/RlServerIndependenceTests.cs
+++ Tests/EditMode/RlServerIndependenceTests.cs
@@ -10,15 +10,15 @@ namespace Bees.Tests.EditMode
     public class RlServerIndependenceTests
     {
         [Test]
-        public void ManagedRemoteTrainingUsesPrivateLoopbackSettingsEndpoint()
+        public void DevelopmentAndTrainingUseVerifiedPublicServerEndpoint()
         {
             string config = ReadSource("Scripts", "ConfigData.cs");
-            Assert.That(config, Does.Contain("BEES_TRAINING_GAMEPLAY_HOST"));
-            Assert.That(config, Does.Contain("BEES_TRAINING_GAMEPLAY_PORT"));
-            Assert.That(config, Does.Contain("TryGetManagedTrainingServerEndpoint"));
-            Assert.That(config, Does.Contain("new Socket(managedPort, managedHost, UseWebSocketSharp)"));
-            Assert.That(config, Does.Contain("DevelopmentServerHostname"),
-                "Ordinary development gameplay must retain the normal public/development endpoint.");
+            Assert.That(config, Does.Contain("LocalServerHostname = \"seagrams7.softether.net\""));
+            Assert.That(config, Does.Contain("DevelopmentServerHostname = LocalServerHostname"));
+            Assert.That(config, Does.Contain("TestServerHostname = LocalServerHostname"));
+            Assert.That(config, Does.Contain("wss://seagrams7.softether.net/bees-ws/"));
+            Assert.That(config, Does.Not.Contain("BEES_TRAINING_GAMEPLAY_HOST"));
+            Assert.That(config, Does.Not.Contain("BEES_TRAINING_GAMEPLAY_PORT"));
         }
 
         [Test]
