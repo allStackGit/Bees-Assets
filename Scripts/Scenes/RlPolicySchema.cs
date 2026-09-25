74af66de1adc16a90ac6f80bcd662e866c77f3b9
allStackGit <dmoran201@protonmail.com>
2026-09-25 08:53:19 -0400

    Bump RL policy ABI for healing weapon exclusivity

--- Scripts/Scenes/RlPolicySchema.cs
+++ Scripts/Scenes/RlPolicySchema.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 /// </summary>
 internal static partial class RlPolicySchema
 {
-    internal const int Version = 19;
+    internal const int Version = 20;
     internal const string ExpectedBehaviorName = "BeesRL1v1";
     internal const int PerceptionObservationSize = 7593;
     internal const int EpisodeProgressObservationIndex = PerceptionObservationSize;
@@ -26,8 +26,8 @@ internal static partial class RlPolicySchema
     internal const int ExpectedSpecialActionBranchSize = 5;
 
     internal const string Signature =
-        "bees-rl-v19|behavior=BeesRL1v1|network=ff-128x3|normalize=true|obs=7614|tail=episode-progress+20-reserved|cont=16|disc=2x5,5|" +
-        "coord-frame=team-episode-distinct-quarter-turn|weapon-aim=slotwise-xy|weapon-fire=slotwise-cease-or-fire|weapon-ready=rl-latched-until-fire|" +
+        "bees-rl-v20|behavior=BeesRL1v1|network=ff-128x3|normalize=true|obs=7614|tail=episode-progress+20-reserved|cont=16|disc=2x5,5|" +
+        "coord-frame=team-episode-distinct-quarter-turn|weapon-aim=slotwise-xy|weapon-fire=slotwise-cease-or-fire|healing=weapon-exclusive|weapon-ready=rl-latched-until-fire|" +
         "shiptype=fixed-scrambled-scalar24|weapontype=fixed-scrambled-scalar10|mapbits=4|shipmap=v1-0..23|weaponmap=v1-0..9|" +
         "allies=64|enemies=64|weapons=5|entity-weapons=5|enemy-mounts=0|mining=8|map-objects=64|moving-asteroids=48|" +
         "self=25|ship-id=episode-permuted-scalar23|capability=12|parent-carrier=40|entity-core=14|entity=40|ally=44-with-private-comm4|communication=4-continuous-private-allied|self-weapon=15|observed-weapon=5|weapon-observation=split-self-vs-observed|mining-slot=7|" +
